724f38ff38db805a42f40c159f853b01fca8bcbf
Nicholas Marchio <namarchio@gmail.com>
2019-12-11 11:12:16 -0600

    Update beta-frame.sql

--- modeling-frame/beta-frame.sql
+++ modeling-frame/beta-frame.sql
@@ -1,4 +1,132 @@
 
+create temp table all_scores_impute as (
+select 
+census_block_group_2010
+,avg(civis_2020_race_native) as avg_civis_2020_race_native
+,avg(civis_2020_race_black) as avg_civis_2020_race_black
+,avg(civis_2020_race_latinx) as avg_civis_2020_race_latinx
+,avg(civis_2020_race_asian) as avg_civis_2020_race_asian
+,avg(civis_2020_race_white) as avg_civis_2020_race_white
+,avg(civis_2020_likely_race) as avg_civis_2020_likely_race
+,avg(civis_2020_likely_race_confidence) as avg_civis_2020_likely_race_confidence
+,avg(civis_2020_subeth_african_american) as avg_civis_2020_subeth_african_american
+,avg(civis_2020_subeth_west_indian) as avg_civis_2020_subeth_west_indian
+,avg(civis_2020_subeth_haitian) as avg_civis_2020_subeth_haitian
+,avg(civis_2020_subeth_african) as avg_civis_2020_subeth_african
+,avg(civis_2020_subeth_other_black) as avg_civis_2020_subeth_other_black
+,avg(civis_2020_subeth_mexican) as avg_civis_2020_subeth_mexican
+,avg(civis_2020_subeth_cuban) as avg_civis_2020_subeth_cuban
+,avg(civis_2020_subeth_puerto_rican) as avg_civis_2020_subeth_puerto_rican
+,avg(civis_2020_subeth_dominican) as avg_civis_2020_subeth_dominican
+,avg(civis_2020_subeth_other_latin_american) as avg_civis_2020_subeth_other_latin_american
+,avg(civis_2020_subeth_other_hispanic) as avg_civis_2020_subeth_other_hispanic
+,avg(civis_2020_subeth_chinese) as avg_civis_2020_subeth_chinese
+,avg(civis_2020_subeth_indian) as avg_civis_2020_subeth_indian
+,avg(civis_2020_subeth_filipino) as avg_civis_2020_subeth_filipino
+,avg(civis_2020_subeth_japanese) as avg_civis_2020_subeth_japanese
+,avg(civis_2020_subeth_vietnamese) as avg_civis_2020_subeth_vietnamese
+,avg(civis_2020_subeth_korean) as avg_civis_2020_subeth_korean
+,avg(civis_2020_subeth_other_asian) as avg_civis_2020_subeth_other_asian
+,avg(civis_2020_subeth_hmong) as avg_civis_2020_subeth_hmong
+,avg(civis_2020_partisanship) as avg_civis_2020_partisanship
+,avg(civis_2020_ideology_liberal) as avg_civis_2020_ideology_liberal
+,avg(civis_2020_spanish_language_preference) as avg_civis_2020_spanish_language_preference
+,avg(civis_2020_cultural_religion_jewish) as avg_civis_2020_cultural_religion_jewish
+,avg(civis_2020_cultural_religion_mormon) as avg_civis_2020_cultural_religion_mormon
+,avg(civis_2020_cultural_religion_muslim) as avg_civis_2020_cultural_religion_muslim
+,avg(civis_2020_cultural_religion_catholic) as avg_civis_2020_cultural_religion_catholic
+,avg(civis_2020_cultural_religion_evangelical) as avg_civis_2020_cultural_religion_evangelical
+,avg(civis_2020_cultural_religion_mainline_protestant) as avg_civis_2020_cultural_religion_mainline_protestant
+,avg(civis_2020_cultural_religion_hindu) as avg_civis_2020_cultural_religion_hindu
+,avg(civis_2020_cultural_religion_buddhist) as avg_civis_2020_cultural_religion_buddhist
+,avg(civis_2020_marriage) as avg_civis_2020_marriage
+,avg(civis_2020_children_present) as avg_civis_2020_children_present
+,avg(dnc_2018_college_graduate) as avg_dnc_2018_college_graduate
+,avg(dnc_2018_marriage) as avg_dnc_2018_marriage
+,avg(dnc_2018_income_dollars) as avg_dnc_2018_income_dollars
+,avg(dnc_2018_downballot_defection_rank) as avg_dnc_2018_downballot_defection_rank
+,avg(dnc_2018_downballot_vote) as avg_dnc_2018_downballot_vote
+,avg(dnc_2018_ideology_liberal) as avg_dnc_2018_ideology_liberal
+,avg(dnc_2018_ideology_moderate) as avg_dnc_2018_ideology_moderate
+,avg(dnc_2018_ideology_conservative) as avg_dnc_2018_ideology_conservative
+,avg(dnc_2018_campaign_finance) as avg_dnc_2018_campaign_finance
+,avg(dnc_2018_climate_change) as avg_dnc_2018_climate_change
+,avg(dnc_2018_govt_privacy) as avg_dnc_2018_govt_privacy
+,avg(dnc_2018_min_wage) as avg_dnc_2018_min_wage
+,avg(dnc_2018_pro_choice) as avg_dnc_2018_pro_choice
+,avg(dnc_2018_citizenship) as avg_dnc_2018_citizenship
+,avg(dnc_2018_college_funding) as avg_dnc_2018_college_funding
+,avg(dnc_2018_gun_control) as avg_dnc_2018_gun_control
+,avg(dnc_2018_paid_leave) as avg_dnc_2018_paid_leave
+,avg(dnc_2018_progressive_tax) as avg_dnc_2018_progressive_tax
+,avg(dnc_2018_volprop_overall_rank) as avg_dnc_2018_volprop_overall_rank
+,avg(dnc_2018_volprop_phone_rank) as avg_dnc_2018_volprop_phone_rank
+,avg(dnc_2018_volprop_walk_rank) as avg_dnc_2018_volprop_walk_rank
+,avg(civis_2018_turnout) as avg_civis_2018_turnout
+,avg(civis_2018_partisanship) as avg_civis_2018_partisanship
+,avg(civis_2018_spanish_language_preference) as avg_civis_2018_spanish_language_preference
+,avg(civis_2018_gotv) as avg_civis_2018_gotv
+,avg(civis_2018_likely_race) as avg_civis_2018_likely_race
+,avg(civis_2018_likely_race_confidence) as avg_civis_2018_likely_race_confidence
+,avg(civis_2018_ballot_dropoff) as avg_civis_2018_ballot_dropoff
+,avg(civis_2018_congressional_gotv_raw) as avg_civis_2018_congressional_gotv_raw
+,avg(civis_2018_congressional_support) as avg_civis_2018_congressional_support
+,avg(civis_2018_avev) as avg_civis_2018_avev
+,avg(civis_2018_cultural_persuasion) as avg_civis_2018_cultural_persuasion
+,avg(civis_2018_economic_persuasion) as avg_civis_2018_economic_persuasion
+,avg(civis_2018_political_persuasion) as avg_civis_2018_political_persuasion
+,avg(dnc_2018_high_school_only) as avg_dnc_2018_high_school_only
+,avg(dnc_2018_income_rank) as avg_dnc_2018_income_rank
+,avg(dnc_2018_catholic_tier) as avg_dnc_2018_catholic_tier
+,avg(dnc_2018_evangelical_tier) as avg_dnc_2018_evangelical_tier
+,avg(dnc_2018_otherchristian_tier) as avg_dnc_2018_otherchristian_tier
+,avg(dnc_2018_nonchristian_tier) as avg_dnc_2018_nonchristian_tier
+,avg(dnc_2018_party_support_score) as avg_dnc_2018_party_support_score
+,avg(civis_2018_gotv_raw) as avg_civis_2018_gotv_raw
+,avg(civis_2018_one_pct_persuasion) as avg_civis_2018_one_pct_persuasion
+,avg(civis_2018_infrastructure_persuasion) as avg_civis_2018_infrastructure_persuasion
+,avg(civis_2018_bipartisan_persuasion) as avg_civis_2018_bipartisan_persuasion
+,avg(civis_2018_aca_persuasion) as avg_civis_2018_aca_persuasion
+,avg(civis_2018_growth_persuasion) as avg_civis_2018_growth_persuasion
+,avg(civis_2018_sexual_assault_persuasion) as avg_civis_2018_sexual_assault_persuasion
+,avg(civis_2018_wall_persuasion) as avg_civis_2018_wall_persuasion
+,avg(civis_2018_marijuana_persuasion) as avg_civis_2018_marijuana_persuasion
+,avg(civis_2018_race_persuasion) as avg_civis_2018_race_persuasion
+,avg(civis_2018_welcome_persuasion) as avg_civis_2018_welcome_persuasion
+,avg(civis_2018_trump_persuasion) as avg_civis_2018_trump_persuasion
+,avg(civis_2018_skills_persuasion) as avg_civis_2018_skills_persuasion
+,avg(civis_2018_gop_persuasion) as avg_civis_2018_gop_persuasion
+,avg(civis_2018_climate_persuasion) as avg_civis_2018_climate_persuasion
+,avg(civis_2018_lgbt_persuasion) as avg_civis_2018_lgbt_persuasion
+,avg(civis_2018_college_persuasion) as avg_civis_2018_college_persuasion
+,avg(civis_2018_guns_persuasion) as avg_civis_2018_guns_persuasion
+,avg(civis_2018_dreamers_persuasion) as avg_civis_2018_dreamers_persuasion
+,avg(civis_2018_military_persuasion) as avg_civis_2018_military_persuasion
+,avg(civis_2018_progressive_persuasion) as avg_civis_2018_progressive_persuasion
+,avg(civis_2018_choice_persuasion) as avg_civis_2018_choice_persuasion
+,avg(civis_2018_medicare_persuasion) as avg_civis_2018_medicare_persuasion
+,avg(civis_2018_ballot_completion) as avg_civis_2018_ballot_completion
+,avg(pres_2016_support) as avg_pres_2016_support
+,avg(dnc_2016_party_support_score) as avg_dnc_2016_party_support_score
+,avg(dnc_2016_turnout) as avg_dnc_2016_turnout
+,avg(dnc_2016_ideology_liberal) as avg_dnc_2016_ideology_liberal
+,avg(dnc_2016_ideology_moderate) as avg_dnc_2016_ideology_moderate
+,avg(dnc_2016_ideology_conservative) as avg_dnc_2016_ideology_conservative
+,avg(dnc_2016_high_school_only) as avg_dnc_2016_high_school_only
+,avg(dnc_2016_income_rank) as avg_dnc_2016_income_rank
+,avg(tsmart_2016_donor_likelihood) as avg_tsmart_2016_donor_likelihood
+from 
+phoenix_analytics.person
+left join
+phoenix_scores.all_scores_2020 using(person_id)
+left join 
+phoenix_scores.all_scores_2018 using(person_id)
+left join 
+phoenix_scores.all_scores_2016 using(person_id)
+group by census_block_group_2010
+)
+
+
 set query_group to 'importers';
 
 drop table if exists bernie_data_commons.phoenix_modeling_frame; 
@@ -10,48 +138,6 @@ as (
 select p.person_id
   ,x.jsonid
   
-  ,coalesce(pri.primary16_clinton,.5) as primary16_clinton
-  ,coalesce(pri.primary16_sanders,.5) as primary16_sanders
-  ,coalesce(dnc_2020_volprop_overall_rank ,0.5) as dnc_2020_volprop_overall_rank
-  ,coalesce(civis_2020_race_native ,0.5) as civis_2020_race_native
-  ,coalesce(civis_2020_race_black ,0.5) as civis_2020_race_black
-  ,coalesce(civis_2020_race_latinx ,0.5) as civis_2020_race_latinx
-  ,coalesce(civis_2020_race_asian ,0.5) as civis_2020_race_asian
-  ,coalesce(civis_2020_race_white ,0.5) as civis_2020_race_white
-  ,coalesce(civis_2020_likely_race ,0.5) as civis_2020_likely_race
-  ,coalesce(civis_2020_likely_race_confidence ,0.5) as civis_2020_likely_race_confidence
-  ,coalesce(civis_2020_subeth_african_american ,0.5) as civis_2020_subeth_african_american
-  ,coalesce(civis_2020_subeth_west_indian ,0.5) as civis_2020_subeth_west_indian
-  ,coalesce(civis_2020_subeth_haitian ,0.5) as civis_2020_subeth_haitian
-  ,coalesce(civis_2020_subeth_african ,0.5) as civis_2020_subeth_african
-  ,coalesce(civis_2020_subeth_other_black ,0.5) as civis_2020_subeth_other_black
-  ,coalesce(civis_2020_subeth_mexican ,0.5) as civis_2020_subeth_mexican
-  ,coalesce(civis_2020_subeth_cuban ,0.5) as civis_2020_subeth_cuban
-  ,coalesce(civis_2020_subeth_puerto_rican ,0.5) as civis_2020_subeth_puerto_rican
-  ,coalesce(civis_2020_subeth_dominican ,0.5) as civis_2020_subeth_dominican
-  ,coalesce(civis_2020_subeth_other_latin_american ,0.5) as civis_2020_subeth_other_latin_american
-  ,coalesce(civis_2020_subeth_other_hispanic ,0.5) as civis_2020_subeth_other_hispanic
-  ,coalesce(civis_2020_subeth_chinese ,0.5) as civis_2020_subeth_chinese
-  ,coalesce(civis_2020_subeth_indian ,0.5) as civis_2020_subeth_indian
-  ,coalesce(civis_2020_subeth_filipino ,0.5) as civis_2020_subeth_filipino
-  ,coalesce(civis_2020_subeth_japanese ,0.5) as civis_2020_subeth_japanese
-  ,coalesce(civis_2020_subeth_vietnamese ,0.5) as civis_2020_subeth_vietnamese
-  ,coalesce(civis_2020_subeth_korean ,0.5) as civis_2020_subeth_korean
-  ,coalesce(civis_2020_subeth_other_asian ,0.5) as civis_2020_subeth_other_asian
-  ,coalesce(civis_2020_subeth_hmong ,0.5) as civis_2020_subeth_hmong
-  ,coalesce(civis_2020_partisanship ,0.5) as civis_2020_partisanship
-  ,coalesce(civis_2020_ideology_liberal ,0.5) as civis_2020_ideology_liberal
-  ,coalesce(civis_2020_spanish_language_preference ,0.5) as civis_2020_spanish_language_preference
-  ,coalesce(civis_2020_cultural_religion_jewish ,0.5) as civis_2020_cultural_religion_jewish
-  ,coalesce(civis_2020_cultural_religion_mormon ,0.5) as civis_2020_cultural_religion_mormon
-  ,coalesce(civis_2020_cultural_religion_muslim ,0.5) as civis_2020_cultural_religion_muslim
-  ,coalesce(civis_2020_cultural_religion_catholic ,0.5) as civis_2020_cultural_religion_catholic
-  ,coalesce(civis_2020_cultural_religion_evangelical ,0.5) as civis_2020_cultural_religion_evangelical
-  ,coalesce(civis_2020_cultural_religion_mainline_protestant ,0.5) as civis_2020_cultural_religion_mainline_protestant
-  ,coalesce(civis_2020_cultural_religion_hindu ,0.5) as civis_2020_cultural_religion_hindu
-  ,coalesce(civis_2020_cultural_religion_buddhist ,0.5) as civis_2020_cultural_religion_buddhist
-  ,coalesce(civis_2020_marriage ,0.5) as civis_2020_marriage
-  ,coalesce(civis_2020_children_present ,0.5) as civis_2020_children_present
 
 -- Urbanity 
   ,case 
@@ -208,6 +294,8 @@ select p.person_id
 from phoenix_analytics.person p 
 left join phoenix_analytics.person_votes pv using(person_id)
 left join phoenix_scores.all_scores_2020 score using(person_id) 
+left join phoenix_scores.all_scores_2018 score using(person_id) 
+left join phoenix_scores.all_scores_2016 score using(person_id) 
 left join phoenix_consumer.tsmart_consumer tc using(person_id) 
 
 left join bernie_data_commons.master_xwalk_dnc x using(person_id)
