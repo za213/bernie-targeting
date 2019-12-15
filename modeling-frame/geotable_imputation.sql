96e139a94506299d512d5731128098ae542b46a6
Nicholas Marchio <namarchio@gmail.com>
2019-12-15 12:48:50 -0600

    Create geotable_imputation.sql

---
+++ modeling-frame/geotable_imputation.sql
@@ -0,0 +1,301 @@
+select
+avg(males_acs_13_17) as males_acs_13_17
+,avg(females_acs_13_17) as females_acs_13_17
+,avg(median_age_acs_13_17) as median_age_acs_13_17
+,avg(pop_under_5_acs_13_17) as pop_under_5_acs_13_17
+,avg(pop_5_17_acs_13_17) as pop_5_17_acs_13_17
+,avg(pop_18_24_acs_13_17) as pop_18_24_acs_13_17
+,avg(pop_25_44_acs_13_17) as pop_25_44_acs_13_17
+,avg(pop_45_64_acs_13_17) as pop_45_64_acs_13_17
+,avg(pop_65plus_acs_13_17) as pop_65plus_acs_13_17
+,avg(hispanic_acs_13_17) as hispanic_acs_13_17
+,avg(nh_white_alone_acs_13_17) as nh_white_alone_acs_13_17
+,avg(nh_blk_alone_acs_13_17) as nh_blk_alone_acs_13_17
+,avg(nh_aian_alone_acs_13_17) as nh_aian_alone_acs_13_17
+,avg(nh_asian_alone_acs_13_17) as nh_asian_alone_acs_13_17
+,avg(nh_nhopi_alone_acs_13_17) as nh_nhopi_alone_acs_13_17
+,avg(nh_sor_alone_acs_13_17) as nh_sor_alone_acs_13_17
+,avg(pop_5yrs_over_acs_13_17) as pop_5yrs_over_acs_13_17
+,avg(othr_lang_acs_13_17) as othr_lang_acs_13_17
+,avg(pop_25yrs_over_acs_13_17) as pop_25yrs_over_acs_13_17
+,avg(not_hs_grad_acs_13_17) as not_hs_grad_acs_13_17
+,avg(college_acs_13_17) as college_acs_13_17
+,avg(pov_univ_acs_13_17) as pov_univ_acs_13_17
+,avg(prs_blw_pov_lev_acs_13_17) as prs_blw_pov_lev_acs_13_17
+,avg(one_health_ins_acs_13_17) as one_health_ins_acs_13_17
+,avg(two_plus_health_ins_acs_13_17) as two_plus_health_ins_acs_13_17
+,avg(no_health_ins_acs_13_17) as no_health_ins_acs_13_17
+,avg(pop_1yr_over_acs_13_17) as pop_1yr_over_acs_13_17
+,avg(diff_hu_1yr_ago_acs_13_17) as diff_hu_1yr_ago_acs_13_17
+,avg(eng_vw_span_acs_13_17) as eng_vw_span_acs_13_17
+,avg(eng_vw_indo_euro_acs_13_17) as eng_vw_indo_euro_acs_13_17
+,avg(eng_vw_api_acs_13_17) as eng_vw_api_acs_13_17
+,avg(eng_vw_other_acs_13_17) as eng_vw_other_acs_13_17
+,avg(eng_vw_acs_13_17) as eng_vw_acs_13_17
+,avg(rel_family_hhd_acs_13_17) as rel_family_hhd_acs_13_17
+,avg(mrdcple_fmly_hhd_acs_13_17) as mrdcple_fmly_hhd_acs_13_17
+,avg(not_mrdcple_hhd_acs_13_17) as not_mrdcple_hhd_acs_13_17
+,avg(female_no_hb_acs_13_17) as female_no_hb_acs_13_17
+,avg(nonfamily_hhd_acs_13_17) as nonfamily_hhd_acs_13_17
+,avg(sngl_prns_hhd_acs_13_17) as sngl_prns_hhd_acs_13_17
+,avg(hhd_ppl_und_18_acs_13_17) as hhd_ppl_und_18_acs_13_17
+,avg(tot_prns_in_hhd_acs_13_17) as tot_prns_in_hhd_acs_13_17
+,avg(rel_child_under_6_acs_13_17) as rel_child_under_6_acs_13_17
+,avg(hhd_moved_in_acs_13_17) as hhd_moved_in_acs_13_17
+,avg(pub_asst_inc_acs_13_17) as pub_asst_inc_acs_13_17
+,avg(med_hhd_inc_bg_acs_13_17) as med_hhd_inc_bg_acs_13_17
+,avg(med_hhd_inc_tr_acs_13_17) as med_hhd_inc_tr_acs_13_17
+,avg(aggregate_hh_inc_acs_13_17) as aggregate_hh_inc_acs_13_17
+,avg(tot_housing_units_acs_13_17) as tot_housing_units_acs_13_17
+,avg(tot_occp_units_acs_13_17) as tot_occp_units_acs_13_17
+,avg(tot_vacant_units_acs_13_17) as tot_vacant_units_acs_13_17
+,avg(renter_occp_hu_acs_13_17) as renter_occp_hu_acs_13_17
+,avg(owner_occp_hu_acs_13_17) as owner_occp_hu_acs_13_17
+,avg(single_unit_acs_13_17) as single_unit_acs_13_17
+,avg(mlt_u2_9_strc_acs_13_17) as mlt_u2_9_strc_acs_13_17
+,avg(mlt_u10p_acs_13_17) as mlt_u10p_acs_13_17
+,avg(mobile_homes_acs_13_17) as mobile_homes_acs_13_17
+,avg(crowd_occp_u_acs_13_17) as crowd_occp_u_acs_13_17
+,avg(occp_u_no_ph_srvc_acs_13_17) as occp_u_no_ph_srvc_acs_13_17
+,avg(no_plumb_acs_13_17) as no_plumb_acs_13_17
+,avg(recent_built_hu_acs_13_17) as recent_built_hu_acs_13_17
+,avg(med_house_value_bg_acs_13_17) as med_house_value_bg_acs_13_17
+,avg(med_house_value_tr_acs_13_17) as med_house_value_tr_acs_13_17
+,avg(aggr_house_value_acs_13_17) as aggr_house_value_acs_13_17
+,avg(mail_return_rate_cen_2010) as mail_return_rate_cen_2010
+,avg(low_response_score) as low_response_score
+,avg(pct_hispanic_acs_13_17) as pct_hispanic_acs_13_17
+,avg(pct_nh_white_alone_acs_13_17) as pct_nh_white_alone_acs_13_17
+,avg(pct_nh_blk_alone_acs_13_17) as pct_nh_blk_alone_acs_13_17
+,avg(pct_nh_aian_alone_acs_13_17) as pct_nh_aian_alone_acs_13_17
+,avg(pct_nh_asian_alone_acs_13_17) as pct_nh_asian_alone_acs_13_17
+,avg(pct_nh_nhopi_alone_acs_13_17) as pct_nh_nhopi_alone_acs_13_17
+,avg(pct_nh_sor_alone_acs_13_17) as pct_nh_sor_alone_acs_13_17
+,avg(pct_pop_5yrs_over_acs_13_17) as pct_pop_5yrs_over_acs_13_17
+,avg(pct_othr_lang_acs_13_17) as pct_othr_lang_acs_13_17
+,avg(pct_pop_25yrs_over_acs_13_17) as pct_pop_25yrs_over_acs_13_17
+,avg(pct_not_hs_grad_acs_13_17) as pct_not_hs_grad_acs_13_17
+,avg(pct_college_acs_13_17) as pct_college_acs_13_17
+,avg(pct_pov_univ_acs_13_17) as pct_pov_univ_acs_13_17
+,avg(pct_prs_blw_pov_lev_acs_13_17) as pct_prs_blw_pov_lev_acs_13_17
+,avg(pct_one_health_ins_acs_13_17) as pct_one_health_ins_acs_13_17
+,avg(pct_twophealthins_acs_13_17) as pct_twophealthins_acs_13_17
+,avg(pct_no_health_ins_acs_13_17) as pct_no_health_ins_acs_13_17
+,avg(pct_pop_1yr_over_acs_13_17) as pct_pop_1yr_over_acs_13_17
+,avg(pct_diff_hu_1yr_ago_acs_13_17) as pct_diff_hu_1yr_ago_acs_13_17
+,avg(pct_eng_vw_span_acs_13_17) as pct_eng_vw_span_acs_13_17
+,avg(pct_eng_vw_indoeuro_acs_13_17) as pct_eng_vw_indoeuro_acs_13_17
+,avg(pct_eng_vw_api_acs_13_17) as pct_eng_vw_api_acs_13_17
+,avg(pct_eng_vw_other_acs_13_17) as pct_eng_vw_other_acs_13_17
+,avg(pct_eng_vw_acs_13_17) as pct_eng_vw_acs_13_17
+,avg(pct_rel_family_hhd_acs_13_17) as pct_rel_family_hhd_acs_13_17
+,avg(pct_mrdcple_hhd_acs_13_17) as pct_mrdcple_hhd_acs_13_17
+,avg(pct_not_mrdcple_hhd_acs_13_17) as pct_not_mrdcple_hhd_acs_13_17
+,avg(pct_female_no_hb_acs_13_17) as pct_female_no_hb_acs_13_17
+,avg(pct_nonfamily_hhd_acs_13_17) as pct_nonfamily_hhd_acs_13_17
+,avg(pct_sngl_prns_hhd_acs_13_17) as pct_sngl_prns_hhd_acs_13_17
+,avg(pct_hhd_ppl_und_18_acs_13_17) as pct_hhd_ppl_und_18_acs_13_17
+,avg(avg_tot_prns_in_hhd_acs_13_17) as avg_tot_prns_in_hhd_acs_13_17
+,avg(pct_rel_under_6_acs_13_17) as pct_rel_under_6_acs_13_17
+,avg(pct_hhd_moved_in_acs_13_17) as pct_hhd_moved_in_acs_13_17
+,avg(pct_pub_asst_inc_acs_13_17) as pct_pub_asst_inc_acs_13_17
+,avg(avg_agg_hh_inc_acs_13_17) as avg_agg_hh_inc_acs_13_17
+,avg(pct_tot_occp_units_acs_13_17) as pct_tot_occp_units_acs_13_17
+,avg(pct_vacant_units_acs_13_17) as pct_vacant_units_acs_13_17
+,avg(pct_renter_occp_hu_acs_13_17) as pct_renter_occp_hu_acs_13_17
+,avg(pct_owner_occp_hu_acs_13_17) as pct_owner_occp_hu_acs_13_17
+,avg(pct_single_unit_acs_13_17) as pct_single_unit_acs_13_17
+,avg(pct_mlt_u2_9_strc_acs_13_17) as pct_mlt_u2_9_strc_acs_13_17
+,avg(pct_mlt_u10p_acs_13_17) as pct_mlt_u10p_acs_13_17
+,avg(pct_mobile_homes_acs_13_17) as pct_mobile_homes_acs_13_17
+,avg(pct_crowd_occp_u_acs_13_17) as pct_crowd_occp_u_acs_13_17
+,avg(pct_no_ph_srvc_acs_13_17) as pct_no_ph_srvc_acs_13_17
+,avg(pct_no_plumb_acs_13_17) as pct_no_plumb_acs_13_17
+,avg(pct_recent_built_hu_acs_13_17) as pct_recent_built_hu_acs_13_17
+,avg(avg_agg_house_value_acs_13_17) as avg_agg_house_value_acs_13_17
+from
+phoenix_analytics.person p
+left join 
+bernie_nmarchio2.census_pdb_blockgroup pdb_bg
+on p.census_block_group_2010 = lpad(pdb_bg.gidbg, 12,'000000000000') 
+where random() < .30
+and is_deceased = false
+  and reg_record_merged = false
+  and reg_on_current_file = true 
+  and reg_voter_flag = true 
+
+
+  select
+  avg(hhinc_mean2000) as hhinc_mean2000
+  ,avg(mean_commutetime2000) as mean_commutetime2000
+  ,avg(frac_coll_plus2010) as frac_coll_plus2010
+  ,avg(frac_coll_plus2000) as frac_coll_plus2000
+  ,avg(foreign_share2010) as foreign_share2010
+  ,avg(gsmn_math_g3_2013) as gsmn_math_g3_2013
+  ,avg(rent_twobed2015) as rent_twobed2015
+  ,avg(singleparent_share2010) as singleparent_share2010
+  ,avg(singleparent_share1990) as singleparent_share1990
+  ,avg(singleparent_share2000) as singleparent_share2000
+  ,avg(traveltime15_2010) as traveltime15_2010
+  ,avg(mail_return_rate2010) as mail_return_rate2010
+  ,avg(ln_wage_growth_hs_grad) as ln_wage_growth_hs_grad
+  ,avg(jobs_total_5mi_2015) as jobs_total_5mi_2015
+  ,avg(jobs_highpay_5mi_2015) as jobs_highpay_5mi_2015
+  ,avg(popdensity2010) as popdensity2010
+  ,avg(ann_avg_job_growth_2004_2013) as ann_avg_job_growth_2004_2013
+  ,avg(job_density_2013) as job_density_2013
+  ,avg(tract_id_2) as tract_id_2
+  ,avg(kfr_pooled_pooled_p25) as kfr_pooled_pooled_p25
+  ,avg(jail_pooled_pooled_p25) as jail_pooled_pooled_p25
+  ,avg(kfr_black_pooled_p25) as kfr_black_pooled_p25
+  ,avg(kfr_hisp_pooled_p25) as kfr_hisp_pooled_p25
+  ,avg(kfr_white_pooled_p25) as kfr_white_pooled_p25
+  ,avg(jail_black_pooled_p25) as jail_black_pooled_p25
+  ,avg(jail_hisp_pooled_p25) as jail_hisp_pooled_p25
+  ,avg(jail_white_pooled_p25) as jail_white_pooled_p25
+  ,avg(kfr_pooled_female_p25) as kfr_pooled_female_p25
+  ,avg(kfr_pooled_male_p25) as kfr_pooled_male_p25
+  ,avg(jail_pooled_female_p25) as jail_pooled_female_p25
+  ,avg(jail_pooled_male_p25) as jail_pooled_male_p25
+  ,avg(kfr_black_female_p25) as kfr_black_female_p25
+  ,avg(kfr_hisp_female_p25) as kfr_hisp_female_p25
+  ,avg(kfr_white_female_p25) as kfr_white_female_p25
+  ,avg(kfr_black_male_p25) as kfr_black_male_p25
+  ,avg(kfr_hisp_male_p25) as kfr_hisp_male_p25
+  ,avg(kfr_white_male_p25) as kfr_white_male_p25
+  ,avg(jail_black_female_p25) as jail_black_female_p25
+  ,avg(jail_hisp_female_p25) as jail_hisp_female_p25
+  ,avg(jail_white_female_p25) as jail_white_female_p25
+  ,avg(jail_black_male_p25) as jail_black_male_p25
+  ,avg(jail_hisp_male_p25) as jail_hisp_male_p25
+  ,avg(jail_white_male_p25) as jail_white_male_p25
+  ,avg(tot_population_acs_13_17_tract) as tot_population_acs_13_17_tract
+  ,avg(median_age_acs_13_17_tract) as median_age_acs_13_17_tract
+  ,avg(civ_noninst_pop_acs_13_17_tract) as civ_noninst_pop_acs_13_17_tract
+  ,avg(tot_prns_in_hhd_acs_13_17_tract) as tot_prns_in_hhd_acs_13_17_tract
+  ,avg(med_hhd_inc_acs_13_17_tract) as med_hhd_inc_acs_13_17_tract
+  ,avg(aggregate_hh_inc_acs_13_17_tract) as aggregate_hh_inc_acs_13_17_tract
+  ,avg(med_house_value_acs_13_17_tract) as med_house_value_acs_13_17_tract
+  ,avg(pct_college_acs_13_17_tract) as pct_college_acs_13_17_tract
+  ,avg(pct_prs_blw_pov_lev_acs_13_17_tract) as pct_prs_blw_pov_lev_acs_13_17_tract
+  ,avg(pct_no_health_ins_acs_13_17_tract) as pct_no_health_ins_acs_13_17_tract
+  ,avg(pct_civ_unemp_16p_acs_13_17_tract) as pct_civ_unemp_16p_acs_13_17_tract
+  ,avg(pct_pop_disabled_acs_13_17_tract) as pct_pop_disabled_acs_13_17_tract
+  ,avg(pct_children_in_pov_acs_13_17_tract) as pct_children_in_pov_acs_13_17_tract
+  ,avg(pct_nohealthins_u19_acs_13_17_tract) as pct_nohealthins_u19_acs_13_17_tract
+  ,avg(pct_nohealthins1964_acs_13_17_tract) as pct_nohealthins1964_acs_13_17_tract
+  ,avg(pct_nohealthins_65p_acs_13_17_tract) as pct_nohealthins_65p_acs_13_17_tract
+  ,avg(pct_schl_enroll_3_4_acs_13_17_tract) as pct_schl_enroll_3_4_acs_13_17_tract
+  ,avg(pct_pop_nocompdevic_acs_13_17_tract) as pct_pop_nocompdevic_acs_13_17_tract
+  ,avg(pct_pop_w_broadcomp_acs_13_17_tract) as pct_pop_w_broadcomp_acs_13_17_tract
+  ,avg(avg_tot_prns_in_hhd_acs_13_17_tract) as avg_tot_prns_in_hhd_acs_13_17_tract
+  ,avg(pct_pub_asst_inc_acs_13_17_tract) as pct_pub_asst_inc_acs_13_17_tract
+  ,avg(avg_agg_hh_inc_acs_13_17_tract) as avg_agg_hh_inc_acs_13_17_tract
+  ,avg(avg_agg_house_value_acs_13_17_tract) as avg_agg_house_value_acs_13_17_tract
+  ,avg(pct_mrdcple_w_child_acs_13_17_tract) as pct_mrdcple_w_child_acs_13_17_tract
+  ,avg(pct_hhd_nocompdevic_acs_13_17_tract) as pct_hhd_nocompdevic_acs_13_17_tract
+  ,avg(pct_hhd_w_computer_acs_13_17_tract) as pct_hhd_w_computer_acs_13_17_tract
+  ,avg(pct_hhd_w_onlysphne_acs_13_17_tract) as pct_hhd_w_onlysphne_acs_13_17_tract
+  ,avg(pct_hhd_no_internet_acs_13_17_tract) as pct_hhd_no_internet_acs_13_17_tract
+  ,avg(pct_hhd_w_broadband_acs_13_17_tract) as pct_hhd_w_broadband_acs_13_17_tract
+  ,avg(mail_return_rate_cen_2010_tract) as mail_return_rate_cen_2010_tract
+  ,avg(low_response_score_tract) as low_response_score_tract
+  ,avg(self_response_rate_acs_13_17_tract) as self_response_rate_acs_13_17_tract
+  from
+  phoenix_analytics.person p
+  left join 
+  bernie_nmarchio2.geo_tract_covariates t
+  on left(p.census_block_group_2010,11) = t.tract_id
+  where random() < .30
+  and is_deceased = false
+    and reg_record_merged = false
+    and reg_on_current_file = true 
+    and reg_voter_flag = true 
+
+    select
+     avg(cty_exposure_natl_hh_income_at_age_26_p25) as cty_exposure_natl_hh_income_at_age_26_p25
+    ,avg(cty_exposure_effects_natl_hh_income_at_age_26_p75) as cty_exposure_effects_natl_hh_income_at_age_26_p75
+    ,avg(cty_exposure_college_attendance_at_ages_18_23_p25) as cty_exposure_college_attendance_at_ages_18_23_p25
+    ,avg(cty_exposure_college_attendance_at_ages_18_23_p75) as cty_exposure_college_attendance_at_ages_18_23_p75
+    ,avg(cty_exposure_value_of_hh_income_at_age_26_p25) as cty_exposure_value_of_hh_income_at_age_26_p25
+    ,avg(cty_exposure_value_of_hh_income_at_age_26_p75) as cty_exposure_value_of_hh_income_at_age_26_p75
+    ,avg(cty_exposure_marriage_rate_at_age_26_p25) as cty_exposure_marriage_rate_at_age_26_p25
+    ,avg(cty_exposure_marriage_rate_at_age_26_p75) as cty_exposure_marriage_rate_at_age_26_p75
+    ,avg(racial_segregation) as racial_segregation
+    ,avg(income_segregation) as income_segregation
+    ,avg(segregation_of_poverty_p25) as segregation_of_poverty_p25
+    ,avg(segregation_of_poverty_p75) as segregation_of_poverty_p75
+    ,avg(fraction_with_commute_less15_mins) as fraction_with_commute_less15_mins
+    ,avg(household_income_per_capita) as household_income_per_capita
+    ,avg(gini) as gini
+    ,avg(top_1pct_income_share) as top_1pct_income_share
+    ,avg(fraction_middle_class_between_p25_and_p75) as fraction_middle_class_between_p25_and_p75
+    ,avg(local_tax_rate) as local_tax_rate
+    ,avg(local_tax_rate_per_capita) as local_tax_rate_per_capita
+    ,avg(local_govt_expenditures_per_capita) as local_govt_expenditures_per_capita
+    ,avg(state_eitc_exposure) as state_eitc_exposure
+    ,avg(tax_progressivity) as tax_progressivity
+    ,avg(school_expenditure_per_student) as school_expenditure_per_student
+    ,avg(student_teacher_ratio) as student_teacher_ratio
+    ,avg(test_score_percentile_income_adjusted) as test_score_percentile_income_adjusted
+    ,avg(high_school_dropout_rate_income_adjusted) as high_school_dropout_rate_income_adjusted
+    ,avg(number_of_colleges_per_capita) as number_of_colleges_per_capita
+    ,avg(college_tuition) as college_tuition
+    ,avg(college_graduation_rate_income_adjusted) as college_graduation_rate_income_adjusted
+    ,avg(labor_force_participation) as labor_force_participation
+    ,avg(share_working_in_manufacturing) as share_working_in_manufacturing
+    ,avg(teenage_14_16_labor_force_participation) as teenage_14_16_labor_force_participation
+    ,avg(migration_inflow_rate) as migration_inflow_rate
+    ,avg(migration_outlflow_rate) as migration_outlflow_rate
+    ,avg(fraction_foreign_born) as fraction_foreign_born
+    ,avg(social_capital_index) as social_capital_index
+    ,avg(fraction_religious) as fraction_religious
+    ,avg(violent_crime_rate) as violent_crime_rate
+    ,avg(total_crime_rate) as total_crime_rate
+    ,avg(fraction_of_children_with_single_mothers) as fraction_of_children_with_single_mothers
+    ,avg(fraction_of_adults_divorced) as fraction_of_adults_divorced
+    ,avg(fraction_of_adults_married) as fraction_of_adults_married
+    ,avg(median_house_price_for_below_median_income_families) as median_house_price_for_below_median_income_families
+    ,avg(median_house_price_for_above_median_income_families) as median_house_price_for_above_median_income_families
+    ,avg(median_house_rent_for_below_median_income_families) as median_house_rent_for_below_median_income_families
+    ,avg(median_house_rent_for_above_median_income_families) as median_house_rent_for_above_median_income_families
+    ,avg(log_population_density) as log_population_density
+    ,avg(location_affordability_of_very_low_income_individual) as location_affordability_of_very_low_income_individual
+    ,avg(location_affordability_of_median_income_family) as location_affordability_of_median_income_family
+    ,avg(unemployment_rate) as unemployment_rate
+    ,avg(fraction_black) as fraction_black
+    ,avg(poverty_rate) as poverty_rate
+    ,avg(segregation_of_affluence_p75) as segregation_of_affluence_p75
+    ,avg(fraction_with_commute__15_mins) as fraction_with_commute__15_mins
+    ,avg(location_affordability_of_median_household) as location_affordability_of_median_household
+    ,avg(fraction_of_parents_in_1st_national_income_decile) as fraction_of_parents_in_1st_national_income_decile
+    ,avg(fraction_of_parents_in_2nd_national_income_decile) as fraction_of_parents_in_2nd_national_income_decile
+    ,avg(fraction_of_parents_in_3rd_national_income_decile) as fraction_of_parents_in_3rd_national_income_decile
+    ,avg(fraction_of_parents_in_4th_national_income_decile) as fraction_of_parents_in_4th_national_income_decile
+    ,avg(fraction_of_parents_in_5th_national_income_decile) as fraction_of_parents_in_5th_national_income_decile
+    ,avg(fraction_of_parents_in_6th_national_income_decile) as fraction_of_parents_in_6th_national_income_decile
+    ,avg(fraction_of_parents_in_7th_national_income_decile) as fraction_of_parents_in_7th_national_income_decile
+    ,avg(fraction_of_parents_in_8th_national_income_decile) as fraction_of_parents_in_8th_national_income_decile
+    ,avg(fraction_of_parents_in_9th_national_income_decile) as fraction_of_parents_in_9th_national_income_decile
+    ,avg(fraction_of_parents_in_10th_national_income_decile) as fraction_of_parents_in_10th_national_income_decile
+    ,avg(w2_pos_30_q1_f) as w2_pos_30_q1_f
+    ,avg(w2_pos_30_q1_m) as w2_pos_30_q1_m
+    ,avg(w2_pos_30_q2_f) as w2_pos_30_q2_f
+    ,avg(w2_pos_30_q2_m) as w2_pos_30_q2_m
+    ,avg(w2_pos_30_q3_f) as w2_pos_30_q3_f
+    ,avg(w2_pos_30_q3_m) as w2_pos_30_q3_m
+    ,avg(w2_pos_30_q4_f) as w2_pos_30_q4_f
+    ,avg(w2_pos_30_q4_m) as w2_pos_30_q4_m
+    ,avg(primary16_clinton) as primary16_clinton
+    ,avg(primary16_sanders) as primary16_sanders
+    ,avg(no_college) as no_college
+    from
+    phoenix_analytics.person p
+    left join 
+    bernie_nmarchio2.geo_county_covariates t
+    on left(p.census_block_group_2010,5) = t.county_fip_id
+    where random() < .30
+    and is_deceased = false
+      and reg_record_merged = false
+      and reg_on_current_file = true 
+      and reg_voter_flag = true 
