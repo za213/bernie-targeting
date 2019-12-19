bedd5b7b8b262ee14fcdffa0d759104a5e07b4e7
Nicholas Marchio <namarchio@gmail.com>
2019-12-19 13:33:36 -0500

    Update voluntee-dv-recode-v2.sql

--- pipeline-etl/voluntee-dv-recode-v2.sql
+++ pipeline-etl/voluntee-dv-recode-v2.sql
@@ -5,6 +5,7 @@ CREATE TABLE bernie_nmarchio2.volunteer_dvs distkey(person_id) sortkey(person_id
 	   WHEN attendees.user_id IS NOT NULL THEN 1
        ELSE 0 END AS attendee_host_ever
 , CASE WHEN attendees.user_id IS NOT NULL THEN 1 ELSE 0 END AS attendee_ever
+, CASE WHEN event.user_id IS NOT NULL THEN 1 ELSE 0 END as event_ever
 FROM bernie_jshuman.donor_basetable base
 LEFT JOIN phoenix_analytics.person p ON p.person_id = base.person_id
 LEFT JOIN 
@@ -27,7 +28,24 @@ LEFT JOIN
         WHERE TRIM(ec.title) <> 'Event with Bernie Sanders'
         AND es.role = 'attendee'
     ) attendees ON attendees.user_id = base.user_id
-JOIN bernie_jshuman.fifteen_day_actives fda on base.user_id = fda.user_id
+LEFT JOIN 
+    (
+        SELECT DISTINCT ca.user_id
+        FROM ak_bernie.core_action ca
+        JOIN ak_bernie.core_eventcreateaction eca ON ca.id = eca.action_ptr_id
+        JOIN ak_bernie.events_event ee ON eca.event_id = ee.id
+        JOIN ak_bernie.events_campaign ec ON ee.campaign_id = ec.id
+        WHERE TRIM(ec.title) <> 'Event with Bernie Sanders'
+    ) hosts ON hosts.user_id = base.user_id 
+ LEFT JOIN 
+    (
+        SELECT DISTINCT ca.user_id
+        FROM ak_bernie.core_action ca
+        JOIN ak_bernie.core_eventcreateaction eca ON ca.id = eca.action_ptr_id
+        JOIN ak_bernie.events_event ee ON eca.event_id = ee.id
+        JOIN ak_bernie.events_campaign ec ON ee.campaign_id = ec.id
+        WHERE TRIM(ec.title) = 'Event with Bernie Sanders'
+    ) event ON event.user_id = base.user_id 
 WHERE RANDOM() < .3
 LIMIT 250000)
 ;
