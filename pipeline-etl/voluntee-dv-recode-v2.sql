25042bc513475f99eef1777d2d76ed4a65b363a7
Nicholas Marchio <namarchio@gmail.com>
2019-12-19 15:32:40 -0500

    Update voluntee-dv-recode-v2.sql

--- pipeline-etl/voluntee-dv-recode-v2.sql
+++ pipeline-etl/voluntee-dv-recode-v2.sql
@@ -5,10 +5,9 @@ create temp table action_pop AS
 	   WHEN attendee = 1 THEN 1
        ELSE 0 END AS attendee_host_ever
 , CASE WHEN attendee = 1 THEN 1 ELSE 0 END AS attendee_ever
+, CASE WHEN rally_attendee = 1 THEN 1 ELSE 0 END AS rally_attendee_ever
 , CASE when n_donations  > 0 then 1 else 0 end as donor_ever
 , CASE when lifetime_value >= 27 then 1 else 0 end as donor_27plus_ever
-, CASE when n_events_attended > 0 then 1 else 0 end as eventattend_ever
-, CASE when  n_event_signups > 0 then 1 else 0 end as eventsignup_ever
 FROM bernie_jshuman.donor_basetable base
 LEFT JOIN phoenix_analytics.person p ON p.person_id = base.person_id
 LEFT JOIN 
@@ -31,13 +30,22 @@ LEFT JOIN
         WHERE TRIM(ec.title) <> 'Event with Bernie Sanders'
         AND es.role = 'attendee'
     ) attendees ON attendees.user_id = base.user_id
+ LEFT JOIN 
+    ( 
+    	SELECT DISTINCT ca.user_id, 1 as rally_attendee
+        FROM ak_bernie.core_action ca
+        JOIN ak_bernie.core_eventsignupaction ces ON ces.action_ptr_id = ca.id
+        JOIN ak_bernie.events_eventsignup es ON es.id = ces.signup_id
+        JOIN ak_bernie.events_event ee ON es.event_id = ee.id
+        JOIN ak_bernie.events_campaign ec ON ee.campaign_id = ec.id
+        WHERE TRIM(ec.title) = 'Event with Bernie Sanders'
+        AND es.role = 'attendee') rally_attendees ON rally_attendees.user_id = base.user_id
 WHERE 
   (lifetime_value > 0 
-or n_event_signups > 0 
-or n_events_attended > 0
 or n_donations > 0
 or host > 0 
-or attendee > 0));
+or attendee > 0
+or rally_attendee > 0));
 
 DROP TABLE IF EXISTS bernie_nmarchio2.action_pop_dvs CASCADE;
 CREATE TABLE bernie_nmarchio2.action_pop_dvs 
@@ -50,14 +58,12 @@ sortkey(person_id) AS
   ,attendee_ever
   ,donor_ever
   ,donor_27plus_ever
-  ,eventattend_ever
-  ,eventsignup_ever
+  ,rally_attendee_ever
   ,case when greatest(attendee_host_ever
                       ,attendee_ever
                       ,donor_ever
                       ,donor_27plus_ever
-                      ,eventattend_ever
-                      ,eventsignup_ever) > 0 then 1 else 0 end as action_ever
+                      ,rally_attendee_ever) > 0 then 1 else 0 end as action_ever
   from action_pop)
  union all
  (select 
