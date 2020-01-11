3c7ba679cc1c9006bf6b4faa685f16f95327bdc6
Nicholas Marchio <namarchio@gmail.com>
2020-01-11 15:00:22 -0600

    Update s1-engagement-universe.sql

--- pipeline-etl/engagement-pipeline/s1-engagement-universe.sql
+++ pipeline-etl/engagement-pipeline/s1-engagement-universe.sql
@@ -101,8 +101,6 @@ CREATE TABLE bernie_nmarchio2.events_users DISTKEY (person_id) AS
          FROM phoenix_analytics.person) p using(person_id) ));
 
 --EVENTS TABLE
-
-
 DROP TABLE IF EXISTS bernie_nmarchio2.events_details;
 CREATE TABLE bernie_nmarchio2.events_details AS
   (SELECT coalesce(ak_event_id,'0')||'_'||coalesce(mobilize_id,'0')||'_'||coalesce(mobilize_timeslot_id,'0')||'_'||coalesce(van_event_van_id,'0')||'_'||coalesce(van_timeslot_id,'0')||'_'||coalesce(event_campaign,'0')||'_'||coalesce(van_location_id,'0')||'_'||coalesce(mob_shift_count,'0')||'_'||coalesce(mob_shift_order,'0') as unique_id ,
