9feb1655cff99aab7e7ebe5db3dc0b986830bddb
Nicholas Marchio <namarchio@gmail.com>
2020-01-14 11:16:30 -0600

    Update rainbow-modeling-frame.sql

--- modeling-frame/rainbow-modeling-frame.sql
+++ modeling-frame/rainbow-modeling-frame.sql
@@ -5,13 +5,13 @@
 set query_group to 'importers';
 set wlm_query_slot_count to 3;
 
-truncate bernie_data_commons.rainbow_modeling_frame;
---drop table if exists bernie_data_commons.rainbow_modeling_frame; 
---create table bernie_data_commons.rainbow_modeling_frame
-insert into bernie_data_commons.rainbow_modeling_frame
---distkey(person_id)
---sortkey(person_id)
---as 
+--truncate bernie_data_commons.rainbow_modeling_frame;
+drop table if exists bernie_data_commons.rainbow_modeling_frame; 
+create table bernie_data_commons.rainbow_modeling_frame
+--insert into bernie_data_commons.rainbow_modeling_frame
+distkey(person_id)
+sortkey(person_id)
+as 
 (select p.person_id
   ,p.state_code
   ,p.census_block_group_2010
