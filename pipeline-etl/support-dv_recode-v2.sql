0630e52bd4f4390afe047d2e45aa264cd85a5e0d
Nicholas Marchio <namarchio@gmail.com>
2020-01-15 14:43:51 -0600

    Update support-dv_recode-v2.sql

--- pipeline-etl/support-dv_recode-v2.sql
+++ pipeline-etl/support-dv_recode-v2.sql
@@ -4,7 +4,8 @@ CREATE TABLE bernie_nmarchio2.support_id_recode
   DISTKEY (person_id)
   SORTKEY (person_id)
 AS 
-(SELECT person_id::varchar(10) ,
+(
+SELECT person_id::varchar(10) ,
        CASE
            WHEN support_int = 1 THEN 1
            WHEN support_int IS NOT NULL
@@ -13,24 +14,33 @@ AS
            ELSE NULL
        END AS support_1_id
 FROM bernie_data_commons.third_party_ids
-WHERE STATE = 'IA'
-  AND datediff(d, TO_DATE(survey_date, 'YYYY-MM-DD'), TO_DATE('2020-01-10','YYYY-MM-DD')) <= 60
-  AND person_id IN
-  (SELECT person_id
+INNER JOIN
+  (SELECT person_id,
+          CASE
+              WHEN (f_ctc_dem = 1
+                    OR f_ctc_last_60_days = 1
+                    OR f_ctc_npp = 1
+                    OR f_ctc_other_party = 1
+                    OR f_id_1_other_party = 1
+                    OR f_donated = 1
+                    OR f_core_donut_top50 = 1
+                    OR f_hosted_1_event = 1
+                    OR f_id_1_dem = 1
+                    OR f_id_1_last_60_days = 1
+                    OR f_id_1_npp = 1
+                    OR f_support_bucket_floor_80 = 1
+                    OR f_support_bucket_floor_90 = 1) THEN 1
+              ELSE 0
+          END AS in_core ,
+          CASE
+              WHEN f_donut_anti_bernie =1
+                   OR field_support_int IN (4,
+                                            5) THEN 1
+              ELSE 0
+          END AS in_exclusion
    FROM gotv_universes.gotv_person_flags
-   WHERE state_code = 'IA'
-     AND (f_ctc_dem = 1
-          OR f_ctc_last_60_days = 1
-          OR f_ctc_npp = 1
-          OR f_ctc_other_party = 1
-          OR f_id_1_other_party = 1
-          OR f_donated = 1
-          OR f_core_donut_top50 = 1
-          OR f_hosted_1_event = 1
-          OR f_id_1_dem = 1
-          OR f_id_1_last_60_days = 1
-          OR f_id_1_npp = 1
-          OR f_support_bucket_floor_80 = 1
-          OR f_support_bucket_floor_90 = 1)
-     AND ((f_donut_anti_bernie = 0 OR f_donut_anti_bernie IS NULL)
-          AND (field_support_int NOT IN (4,5) OR field_support_int IS NULL))));
+   WHERE state_code = 'IA' AND person_id IS NOT NULL) using(person_id)
+WHERE in_core = 0
+  AND in_exclusion = 0
+  AND state = 'IA'
+  AND datediff(d, TO_DATE(survey_date, 'YYYY-MM-DD'), TO_DATE('2020-01-10','YYYY-MM-DD')) <= 60);
