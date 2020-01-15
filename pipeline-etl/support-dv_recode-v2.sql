0eb44f508bd7eb7ddeb336b5846dd543294ebd32
Nicholas Marchio <namarchio@gmail.com>
2020-01-15 17:36:36 -0600

    Update support-dv_recode-v2.sql

--- pipeline-etl/support-dv_recode-v2.sql
+++ pipeline-etl/support-dv_recode-v2.sql
@@ -14,31 +14,32 @@ AS
        END AS support_1_id
 FROM bernie_data_commons.third_party_ids
 INNER JOIN
-  (SELECT person_id,
-          CASE
-              WHEN (f_ctc_dem = 1
-                    OR f_ctc_last_60_days = 1
-                    OR f_ctc_npp = 1
-                    OR f_donated = 1
-                    OR f_ctc_other_party = 1
-                    OR f_core_donut_top50 = 1
-                    OR f_hosted_1_event = 1
-                    OR f_id_1_other_party = 1
-                    OR f_id_1_dem = 1
-                    OR f_id_1_last_60_days = 1
-                    OR f_id_1_npp = 1
-                    OR f_support_bucket_floor_80 = 1
-                    OR f_support_bucket_floor_90 = 1) THEN 1
-              ELSE 0
-          END AS in_core ,
-          CASE
-              WHEN f_donut_anti_bernie =1
-                   OR field_support_int IN (4,
-                                            5) THEN 1
-              ELSE 0
-          END AS in_exclusion
-   FROM gotv_universes.gotv_person_flags
-   WHERE state_code = 'IA' AND person_id IS NOT NULL) using(person_id)
+(SELECT 
+person_id , 
+CASE
+           WHEN (-- f_id_1_other_party = 1
+                 -- OR f_id_1_dem = 1
+                 -- OR f_id_1_last_60_days = 1
+                 -- OR f_id_1_npp = 1
+                 f_ctc_dem = 1
+                 OR f_ctc_last_60_days = 1
+                 OR f_ctc_npp = 1
+                 OR f_ctc_other_party = 1
+                 OR f_donated = 1
+                 OR f_core_donut_top50 = 1
+                 OR f_hosted_1_event = 1
+                 OR f_support_bucket_floor_80 = 1
+                 OR f_support_bucket_floor_90 = 1 ) THEN 1
+           ELSE 0
+       END AS in_core_excl_1s ,
+       CASE
+           WHEN f_donut_anti_bernie = 1 
+           -- OR field_support_int IN (4,5)
+           THEN 1
+           ELSE 0
+       END AS in_exclusion
+FROM gotv_universes.gotv_person_flags
+WHERE state_code = 'IA' AND person_id IS NOT NULL) using(person_id)
 WHERE in_core = 0
   AND in_exclusion = 0
   AND state = 'IA'
