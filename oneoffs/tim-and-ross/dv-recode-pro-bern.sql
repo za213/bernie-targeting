36b4d86d1be80ae6666d81ecbb8675a75e4ed83e
Nicholas Marchio <namarchio@gmail.com>
2020-01-16 11:37:57 -0600

    Update dv-recode-pro-bern.sql

--- oneoffs/tim-and-ross/dv-recode-pro-bern.sql
+++ oneoffs/tim-and-ross/dv-recode-pro-bern.sql
@@ -13,8 +13,8 @@ AS
 ,case when first_choice = 'Pete Buttigieg' then 1 when first_choice is not null or first_choice <> 'Donald Trump' then 0 else NULL end as like_buttigieg
 ,case when first_choice IN ('Kamala Harris','Julian Castro','Andrew Yang','Tulsi Gabbard','Cory Booker','Beto ORourke','Michael Bennett','Deval Patrick','Mike Bloomberg','Amy Klobuchar','Tom Steyer','Other','Someone else','Dont Know') then 1 when first_choice is not null or first_choice <> 'Donald Trump' then 0 else NULL end as like_other
 -- Support ID DVs
-,case when support_int = 1 then 1 when support_int is not null or support_int <> 0 or first_choice <> 'Donald Trump' then 0 else NULL end as pro_bern_1_id
-,case when support_int IN (1,2) then 1 when support_int is not null or support_int <> 0 or first_choice <> 'Donald Trump' then 0 else NULL end as support_1_2_id
+,case when support_int = 1 then 1 when support_int is not null or support_int <> 0 or first_choice <> 'Donald Trump' then 0 else NULL end as pro_bernie_1_id
+,case when support_int IN (1,2) then 1 when support_int is not null or support_int <> 0 or first_choice <> 'Donald Trump' then 0 else NULL end as pro_bernie_1_2_id
 from bernie_data_commons.third_party_ids where state = 'NH');
 
 
@@ -28,11 +28,11 @@ AS
        CASE
            WHEN politicalparty <> 'Republican' AND latestsupportid = 1 THEN 1
            WHEN politicalparty <> 'Republican' AND latestsupportid IN (2,3,4,5) THEN 0
-           ELSE NULL END AS pro_bernie_id_1_no_gop ,
+           ELSE NULL END AS pro_bernie_fieldid_1_no_gop ,
        CASE
            WHEN latestsupportid = 1 THEN 1
            WHEN latestsupportid IN (2,3,4,5) THEN 0
            ELSE NULL END AS pro_bernie_id_1 
-FROM bernie_nh.dim_voters) where pro_bernie_id_1 is not null and pro_bernie_id_1 is not null);
+FROM bernie_nh.dim_voters) where pro_bernie_fieldid_1 is not null and pro_bernie_id_1 is not null);
 	
 	
