c9087445505ee07da83daf6b9909f2c398777432
Nicholas Marchio <namarchio@gmail.com>
2020-01-22 00:28:31 -0500

    Update score-validation.R

--- oneoffs/rando-queries/score-validation.R
+++ oneoffs/rando-queries/score-validation.R
@@ -1,4 +1,3 @@
-
 q3 <- "select 
 case when support_int_field = 1 then 1 else 0 end as support_ind_field_1_0,
 NTILE(100) OVER (ORDER BY support_score) AS support_score_100,
@@ -27,43 +26,37 @@ df3 <- civis::read_civis(sql(q3), database = 'Bernie 2020')
 
 df_test <- df3 %>% 
   mutate(contactdate_field=as.Date(contactdate_field,format="%Y-%m-%d")) %>%
-  filter(support_int_field > 0,
-         state_coalesced %in% c('SC','AL','AR','CA','CO','ME','MA','MN','NC','OK','TN','TX','UT','VT','VA'),
-         contactdate_field > '2019-12-23' )
-#'IA','NH','NV',
+  filter(support_int > 0, #_field
+         state_coalesced %in% c('AL','AR','NC','OK','TN','VA','TX','CO','ME','MA','MN','VT','UT')#,
+         #contactdate_field > '2019-12-23'
+         ) %>%
+  mutate(support_int_1_0 = case_when(support_int == 1 ~ 1,
+                                     support_int == 2 ~ 1,
+                                     TRUE ~ 0))
+
+#  'IA','NH','NV','CA',
+
+round(cor(df_test %>% select(support_ind_field_1_0,support_score,current_support_raw)),2)
 
 #Civis
-roc_obj <- roc(df_test$support_ind_field_1_0, 
-               df_test$support_score)
+roc_obj <- roc(df_test$support_int_1_0, 
+               df_test$support_score) #_100/100
 auc(roc_obj)
+
+ggplot2::ggplot(df_test, ggplot2::aes(support_score_100, color = as.character(support_int_1_0) )) + 
+  ggplot2::geom_density( alpha = .6) +
+  ggplot2::scale_x_continuous( ) +
+  ggplot2::theme_bw() +
+  ggplot2::theme(legend.position ="bottom",
+                 legend.title = ggplot2::element_blank(),
+                 text = ggplot2::element_text(size = 13, color = "#161616"))
 
 #Haystaq 
-roc_obj <- roc(df_test$support_ind_field_1_0, 
-               df_test$current_support_raw/100)
+roc_obj <- roc(df_test$support_int_1_0, 
+               df_test$current_support_raw/100)#_100/100
 auc(roc_obj)
 
-roc_obj <- roc(df_test$support_ind_field_1_0, 
-               df_test$field_id_1_score/100)
-auc(roc_obj)
-
-# Plots
-ggplot2::ggplot(df_test, ggplot2::aes(current_support_raw_100, color = as.character(support_int_field) )) + 
-  ggplot2::geom_density( alpha = .6) +
-  ggplot2::scale_x_continuous( ) +
-  ggplot2::theme_bw() +
-  ggplot2::theme(legend.position ="bottom",
-                 legend.title = ggplot2::element_blank(),
-                 text = ggplot2::element_text(size = 13, color = "#161616"))
-
-ggplot2::ggplot(df_test, ggplot2::aes(field_id_1_score_100, color = as.character(support_int_field) )) + 
-  ggplot2::geom_density( alpha = .6) +
-  ggplot2::scale_x_continuous( ) +
-  ggplot2::theme_bw() +
-  ggplot2::theme(legend.position ="bottom",
-                 legend.title = ggplot2::element_blank(),
-                 text = ggplot2::element_text(size = 13, color = "#161616"))
-
-ggplot2::ggplot(df_test, ggplot2::aes(support_score_100, color = as.character(support_int_field) )) + 
+ggplot2::ggplot(df_test, ggplot2::aes(current_support_raw_100, color = as.character(support_int_1_0) )) + 
   ggplot2::geom_density( alpha = .6) +
   ggplot2::scale_x_continuous( ) +
   ggplot2::theme_bw() +
