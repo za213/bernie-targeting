8b350e76cf41efe2ddd1b6b3ce7e9a8877587fe4
Nicholas Marchio <namarchio@gmail.com>
2020-01-24 19:06:14 -0500

    Update base_query.sql

--- oneoffs/universes/base_query.sql
+++ oneoffs/universes/base_query.sql
@@ -2,54 +2,69 @@
 -- https://github.com/Bernie-2020/universe_review/blob/master/GOTV%20Universes/gotv_person_flags.sql
 -- https://github.com/Bernie-2020/data-analytics/blob/master/ad_hoc/person_primary_votes.sql
 
-
-
-DROP TABLE IF EXISTS bernie_data_commons.march_universe;
-CREATE TABLE bernie_data_commons.march_universe 
-distkey(person_id) sortkey(person_id) AS
+DROP TABLE IF EXISTS bernie_nmarchio2.march_universe;
+CREATE TABLE bernie_nmarchio2.march_universe 
+distkey(person_id) 
+sortkey(person_id) AS
+(SELECT *,
+    -- Rank order each segment
+    row_number() OVER (PARTITION BY state_code ORDER BY bern_flags DESC, gotv_segment ASC, gotv_segment_10 DESC, vote_history ASC, current_support_raw DESC) as gotv_rank -- placeholder swap vote_history with the contact_purpose bucket that orders groups according to chronology of GOTV contact actions
+    FROM
   (SELECT person_id,
           state_code,
+          gotv_segment,
+          -- Decile each segment
+          NTILE(10) OVER (PARTITION BY state_code||'_'||gotv_segment ORDER BY current_support_raw ASC) AS gotv_segment_10,
           bern_flags,
           current_support_raw,
+          current_support_raw_100,
           sanders_very_excited_score,
+          sanders_very_excited_score_100,
           sanders_strong_support_score,
+          sanders_strong_support_score_100,
           field_id_1_score,
-          turnout_current,
+          field_id_1_score_100,
           vote_history,
           party_affiliation,
           registration_action,
           early_vote_mode,
-          absentee_ballot,
-          current_support_raw_100,
-          sanders_very_excited_score_100,
-          sanders_strong_support_score_100,
-          field_id_1_score_100,
-          CASE WHEN bern_flags = 1 THEN 100 ELSE current_support_raw_100 END AS support_targets_100,
-          row_number() OVER (PARTITION BY state_code ORDER BY bern_flags DESC, current_support_raw_100 DESC, turnout_priority ASC, current_support_raw DESC) as rank_order_100,
-          CASE WHEN bern_flags = 1 THEN 20 ELSE current_support_raw_20 END AS support_targets_20,
-          row_number() OVER (PARTITION BY state_code ORDER BY bern_flags DESC, current_support_raw_20 DESC, turnout_priority ASC, current_support_raw DESC) as rank_order_20,
+          registered_in_state,
+          dem_primary_electorate,
+          turnout_current,
+          civis_2018_turnout,
+          ts_tsmart_presidential_primary_turnout_score
    FROM
-
-
-DROP TABLE IF EXISTS bernie_nmarchio2.march_universe;
-CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_id) AS
   (SELECT person_id::varchar,
           state_code,
           current_support_raw,
-          sanders_very_excited_score,
-          sanders_strong_support_score,
-          field_id_1_score,
-          turnout_current,
-          coalesce(bern_flags,0) AS bern_flags,
           NTILE(100) OVER (PARTITION BY state_code ORDER BY current_support_raw ASC) AS current_support_raw_100,
+          sanders_very_excited_score,
           NTILE(100) OVER (PARTITION BY state_code ORDER BY sanders_very_excited_score ASC) AS sanders_very_excited_score_100,
+          sanders_strong_support_score,
           NTILE(100) OVER (PARTITION BY state_code ORDER BY sanders_strong_support_score ASC) AS sanders_strong_support_score_100,
+          field_id_1_score,
           NTILE(100) OVER (PARTITION BY state_code ORDER BY field_id_1_score ASC) AS field_id_1_score_100,
           vote_history,
           party_affiliation,
           registration_action,
           early_vote_mode,
-          absentee_ballot
+          registered_in_state,
+          coalesce(bern_flags,0) AS bern_flags,
+          CASE WHEN (bern_flags = 1 
+                   OR registration_action = '1 - Dem Primary Eligible' 
+                   OR party_affiliation = 'Democratic' 
+                   OR current_support_raw_100 > 70) THEN '1 - Potential GOTV target' -- find flags to remove the hostile people from GOTV universe
+               ELSE '2 - Non-target' END AS dem_primary_electorate,
+          CASE WHEN bern_flags = 1 then '1 - Verified Supporter'
+               WHEN (vote_history = '1 - Newly registered' OR vote_history = '2 - Dem Primary or 2018 voter')
+                   AND registration_action = '1 - Dem Primary Eligible' 
+                   AND registered_in_state = '1 - Registered in current state'
+                   AND (current_support_raw_100 >= 80 AND sanders_very_excited_score_100 >= 80) THEN '2 - Vote-ready Supporter'
+               WHEN dem_primary_electorate = '1 - Potential GOTV target' THEN '3 - Motivate Target'
+               ELSE '4 - Non-target' END AS gotv_segment, -- refine segmenting / this is placeholder
+          turnout_current,
+          civis_2018_turnout,
+          ts_tsmart_presidential_primary_turnout_score
    FROM
      (SELECT person_id::varchar,
              state_code,
@@ -59,7 +74,7 @@ CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_i
              coalesce(field_id_1_score,0) AS field_id_1_score,
              coalesce(turnout_current,0) AS turnout_current
       FROM bernie_data_commons.all_scores
-      WHERE state_code IN ('AL','AR','CA','CO','ME','MA','MN','NC','OK','TN','TX','UT','VA','VT','PR','ID','MI','MS','MO','ND','WA','WY','AZ','FL','IL','OH','GA','ND'))
+      WHERE state_code IN ('SC','NV','AL','AR','CA','CO','ME','MA','MN','NC','OK','TN','TX','UT','VA','VT','PR','ID','MI','MS','MO','ND','WA','WY','AZ','FL','IL','OH','GA','ND'))
    LEFT JOIN
      (SELECT person_id::varchar,
         CASE 
@@ -78,13 +93,13 @@ CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_i
              OR f_ctc_npp = 1
              OR f_donated = 1
              OR f_core_donut_top50 = 1 THEN 1 
-             ELSE 0 END AS bern_flags
+             ELSE 0 END AS bern_flags -- refine logic / this is placeholder
       FROM gotv_universes.gotv_person_flags
       WHERE person_id IS NOT NULL AND bern_flags = 1) using(person_id)
    LEFT JOIN
      (SELECT person_id::varchar,
         CASE 
-           WHEN p.registration_date::date > '2018-11-08' THEN '1 - Primary, new reg, or 2018 voter' 
+           WHEN p.registration_date::date > '2018-11-08' THEN '1 - Newly registered' 
            WHEN (pv.vote_p_2018_party_d = 1
              OR pv.vote_p_2017_party_d = 1 
              OR pv.vote_p_2016_party_d = 1 
@@ -114,8 +129,8 @@ CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_i
              OR pv.vote_pp_1996_party_d = 1 
              OR ppv.voted_16pp_flag = 1 
              OR ppv.voted_08pp_flag = 1 
-             OR ppv.voted_04pp_flag = 1) THEN '1 - Primary, new reg, or 2018 voter'
-           WHEN (pv.vote_g_2018 = 1) THEN '1 - Primary, new reg, or 2018 voter' 
+             OR ppv.voted_04pp_flag = 1) THEN '2 - Dem Primary or 2018 voter'
+           WHEN (pv.vote_g_2018 = 1) THEN '2 - Dem Primary or 2018 voter'
            WHEN (pv.vote_g_2016 = 1
              OR pv.vote_g_2014 = 1
              OR pv.vote_g_2012 = 1
@@ -132,7 +147,7 @@ CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_i
              OR pv.vote_g_2014_novote_eligible = 1
              OR pv.vote_g_2016_novote_eligible = 1
              OR pv.vote_g_2018_novote_eligible = 1) THEN '4 - No vote but eligible' 
-             ELSE '5 - Other' END AS vote_history ,
+             ELSE '5 - Other' END AS vote_history , 
        CASE 
            WHEN p.state_code IN ('AL','GA','HI','IL','IN','MI','MN','MO','MS','MT','ND','OH','SC','TN','TX','VA','VT','WA','WI') AND as20.civis_2020_partisanship >= .75 THEN 'Democratic' 
            WHEN p.state_code IN ('AL','GA','HI','IL','IN','MI','MN','MO','MS','MT','ND','OH','SC','TN','TX','VA','VT','WA','WI') AND (as20.civis_2020_partisanship >= .7 AND as20.civis_2020_partisanship < .75) THEN 'Green' 
@@ -147,15 +162,15 @@ CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_i
            WHEN p.party_name_dnc = 'Independent' OR p.party_id = 6 THEN 'Independent' 
            WHEN p.party_name_dnc = 'Nonpartisan' OR p.party_id = 7 THEN 'Nonpartisan' 
            WHEN p.party_name_dnc = 'Unaffiliated' OR p.party_id = 8 THEN 'Unaffiliated' 
-           ELSE 'Other' END AS party_affiliation ,
+           ELSE 'Other' END AS party_affiliation , -- verify logic
       CASE 
-           WHEN p.state_code IN ('AL','AR','GA','IL','IN','MI','MO','MS','MT','MN','ND','OH','SC','TN','TX','UT','VA','VT','WI','WA') THEN 'Dem Primary Eligible' --open
-           WHEN p.state_code IN ('OK','NE') AND (party_affiliation = 'Democratic' OR party_affiliation = 'Independent') THEN 'Dem Primary Eligible' -- mixed
-           WHEN p.state_code IN ('SD','RI') AND (party_affiliation = 'Democratic' OR party_affiliation = 'Unaffiliated' OR party_affiliation = 'Independent') THEN 'Dem Primary Eligible' -- mixed
-           WHEN p.state_code = 'CA' AND (party_affiliation = 'Democratic' OR party_affiliation = 'Nonpartisan') THEN 'Dem Primary Eligible' -- mixed
-           WHEN p.state_code IN ('CO','ID','MA','NC','NH') AND (party_affiliation = 'Democratic' OR party_affiliation = 'Unaffiliated') THEN 'Dem Primary Eligible' -- mixed
-           WHEN p.state_code IN ('AK','AZ','CT','DC','DE','FL','HI','IA','KS','KY','LA','MD','ME','NJ','NM','NV','NY','OR','PA','WV','WY') AND (party_affiliation = 'Democratic') THEN 'Dem Primary Eligible' -- mixed
-           ELSE 'Must Register as Dem' END AS registration_action ,
+           WHEN p.state_code IN ('AL','AR','GA','IL','IN','MI','MO','MS','MT','MN','ND','OH','SC','TN','TX','UT','VA','VT','WI','WA') THEN '1 - Dem Primary Eligible' --open
+           WHEN p.state_code IN ('OK','NE') AND (party_affiliation = 'Democratic' OR party_affiliation = 'Independent') THEN '1 - Dem Primary Eligible' -- mixed
+           WHEN p.state_code IN ('SD','RI') AND (party_affiliation = 'Democratic' OR party_affiliation = 'Unaffiliated' OR party_affiliation = 'Independent') THEN '1 - Dem Primary Eligible' -- mixed
+           WHEN p.state_code = 'CA' AND (party_affiliation = 'Democratic' OR party_affiliation = 'Nonpartisan') THEN '1 - Dem Primary Eligible' -- mixed
+           WHEN p.state_code IN ('CO','ID','MA','NC','NH') AND (party_affiliation = 'Democratic' OR party_affiliation = 'Unaffiliated') THEN '1 - Dem Primary Eligible' -- mixed
+           WHEN p.state_code IN ('AK','AZ','CT','DC','DE','FL','HI','IA','KS','KY','LA','MD','ME','NJ','NM','NV','NY','OR','PA','WV','WY') AND (party_affiliation = 'Democratic') THEN '1 - Dem Primary Eligible' -- mixed
+           ELSE '2 - Must Register as Dem' END AS registration_action , -- verify logic
       CASE 
            WHEN ((pv.vote_g_2008_method_early = 1 OR pv.vote_p_2008_method_early = 1) AND pev.voted_early_by_mail_2008 = 1)
              OR ((pv.vote_g_2010_method_early = 1 OR pv.vote_p_2010_method_early = 1) AND pev.voted_early_by_mail_2010 = 1)
@@ -194,23 +209,34 @@ CREATE TABLE bernie_nmarchio2.march_universe distkey(person_id) sortkey(person_i
              OR pv.vote_pp_2016_method_early = 1
              OR pv.vote_pp_2012_method_early = 1
              OR pv.vote_pp_2008_method_early = 1) THEN 'EV Mail/In Person' 
-             ELSE 'Non-Early Voter' END AS early_vote_mode ,
+             ELSE 'Non-Early Voter' END AS early_vote_mode , -- verify logic
       CASE 
-           WHEN pv.vote_g_2018_method_absentee = 1 OR pv.vote_p_2018_method_absentee = 1 THEN 'Voted absentee 2018' 
-           WHEN pv.vote_g_2017_method_absentee = 1 OR pv.vote_g_2016_method_absentee = 1 THEN 'Voted absentee 2017' 
-           WHEN pv.vote_p_2016_method_absentee = 1 OR pv.vote_pp_2016_method_absentee = 1 THEN 'Voted absentee 2016'
-           WHEN (poos.from_state_code <> poos.to_state_code AND datediff(d, TO_DATE(poos.ncoa_effective_date, 'YYYY-MM-DD'), TO_DATE(CURRENT_DATE,'YYYY-MM-DD')) < 365) THEN 'Moved in last year' 
-           WHEN (poos.from_state_code <> poos.to_state_code AND datediff(d, TO_DATE(poos.ncoa_effective_date, 'YYYY-MM-DD'), TO_DATE(CURRENT_DATE,'YYYY-MM-DD')) < 730) THEN 'Moved in last two years' 
-           ELSE 'Unlikely absentee' END AS absentee_ballot
+           WHEN poos.to_state_code <> pncoa.state_code 
+             AND (pv.vote_g_2018_method_absentee = 1 
+                OR pv.vote_p_2018_method_absentee = 1 
+                OR pv.vote_g_2017_method_absentee = 1 
+                OR pv.vote_g_2016_method_absentee = 1
+                OR pv.vote_p_2016_method_absentee = 1) THEN '3 - Likely absentee voter'
+           WHEN (poos.to_state_code <> pncoa.state_code) THEN '2 - Registered in different state'
+           ELSE '1 - Registered in current state' END AS registered_in_state, -- verify logic
+
+          as18.civis_2018_turnout,
+          tc.ts_tsmart_presidential_primary_turnout_score
 
       FROM phoenix_analytics.person p
       LEFT JOIN phoenix_scores.all_scores_2020 as20 using(person_id)
+      LEFT JOIN phoenix_scores.all_scores_2018 as18 using(person_id)
+      LEFT JOIN phoenix_analytics.person_ncoas_current pncoa using(person_id)
+      LEFT JOIN phoenix_consumer.tsmart_consumer tc using(person_id)
       LEFT JOIN phoenix_analytics.person_votes pv using(person_id)
       LEFT JOIN phoenix_analytics.person_out_of_state poos using(person_id)
       LEFT JOIN phoenix_analytics.person_early_votes pev using(person_id)
       LEFT JOIN bernie_data_commons.person_primary_votes ppv using(person_id) 
       WHERE person_id IS NOT NULL) using(person_id)) 
-);
+));
+
+
+
 
 
 
