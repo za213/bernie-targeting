a88bcd35ecc4ac33f23da713bf2b95e126b1b693
Nicholas Marchio <namarchio@gmail.com>
2020-01-29 19:56:50 -0500

    Update validation_check.sql

--- oneoffs/universes/validation_check.sql
+++ oneoffs/universes/validation_check.sql
@@ -1,5 +1,4 @@
 
-
 DROP TABLE IF EXISTS bernie_nmarchio2.gotv_validation_check;
 CREATE TABLE bernie_nmarchio2.gotv_validation_check
 distkey(person_id) 
@@ -26,15 +25,16 @@ LEFT JOIN
 -- Third Party 
 (select * from (select *
 ,case 
-WHEN datediff(d, '2020-01-17', TO_DATE(survey_date, 'YYYY-MM-DD')) and state = 'MN' THEN 1 
-WHEN datediff(d, '2020-01-22', TO_DATE(survey_date, 'YYYY-MM-DD')) and state IN ('TN','AR','MO','AL','KY','GA','MS') THEN 1
-WHEN datediff(d, '2020-12-01', TO_DATE(survey_date, 'YYYY-MM-DD')) and state NOT IN ('MN','TN','AR','MO','AL','KY','GA','MS','NH','NV','IA','SC') THEN 1
-WHEN holdout = 1 THEN 1
-ELSE 0 END AS validation_id
+when validtime1 = 1 THEN 1
+WHEN holdout1 = 1 THEN 1
+ELSE 0 END AS validation_id1
  from (select 
  person_id::varchar(10)
- ,survey_date
- ,state
+ ,case 
+WHEN datediff(d, '2020-01-17', TO_DATE(survey_date, 'YYYY-MM-DD')) > 0 and state = 'MN' THEN 1 
+WHEN datediff(d, '2020-01-22', TO_DATE(survey_date, 'YYYY-MM-DD')) > 0 and state IN ('TN','AR','MO','AL','KY','GA','MS') THEN 1
+WHEN datediff(d, '2020-12-01', TO_DATE(survey_date, 'YYYY-MM-DD')) > 0 and state NOT IN ('MN','TN','AR','MO','AL','KY','GA','MS','NH','NV','IA','SC') THEN 1
+ELSE 0 END as validtime1
 -- Candidate Support DVs
 ,case when first_choice = 'Bernie Sanders' then 1 when first_choice is not null or first_choice <> 'Donald Trump' then 0 else NULL end as first_choice_bernie
 ,case when first_choice = 'Donald Trump' then 1 when first_choice is not null then 0 else NULL end as first_choice_trump
@@ -50,8 +50,8 @@ ELSE 0 END AS validation_id
 ,case when support_int IN (1,2) then 1 when support_int is not null or support_int <> 0 or first_choice <> 'Donald Trump' then 0 else NULL end as support_1_2_id
 FROM bernie_data_commons.third_party_ids) third
 LEFT JOIN
-(select person_id::varchar as pid, 1 as holdout from haystaq.set_no_current where set_no=3) ho 
-on third.person_id = ho.pid) where validation_id =1)
+(select person_id::varchar as pid1, 1 as holdout1 from haystaq.set_no_current where set_no=3) ho 
+on third.person_id = ho.pid1) where validation_id1 =1)
 using(person_id)
 
 LEFT JOIN 
@@ -59,15 +59,18 @@ LEFT JOIN
 (select * from 
 (select *,
         case 
-        WHEN datediff(d, '2020-01-17', TO_DATE(survey_date, 'YYYY-MM-DD')) and state = 'MN' THEN 1 
-        WHEN datediff(d, '2020-01-22', TO_DATE(survey_date, 'YYYY-MM-DD')) and state IN ('TN','AR','MO','AL','KY','GA','MS') THEN 1
-        WHEN datediff(d, '2020-12-01', TO_DATE(survey_date, 'YYYY-MM-DD')) and state NOT IN ('MN','TN','AR','MO','AL','KY','GA','MS','NH','NV','IA','SC') THEN 1
-        WHEN holdout = 1 THEN 1
-        ELSE 0 END AS validation_id
+        WHEN  validtime2 = 1 then 1
+        WHEN holdout2 = 1 THEN 1
+        ELSE 0 END AS validation_id2
 FROM 
 (SELECT person_id::varchar , 
         CASE WHEN resultcode IN ('Canvassed', 'Do Not Contact', 'Refused', 'Call Back', 'Language Barrier', 'Hostile', 'Come Back', 'Cultivation', 'Refused Contact', 'Spanish', 'Other', 'Not Interested') THEN 1 ELSE 0 END AS contact_made , 
         CASE WHEN resultcode IN ('Do Not Contact','Hostile','Refused','Refused Contact') OR (support_int = 4 OR support_int = 5) THEN 1 ELSE 0 END AS negative_result ,
+        case 
+        WHEN datediff(d, '2020-01-17', TO_DATE(contactdate, 'YYYY-MM-DD')) > 0 and voter_state = 'MN' THEN 1 
+        WHEN datediff(d, '2020-01-22', TO_DATE(contactdate, 'YYYY-MM-DD')) > 0 and voter_state IN ('TN','AR','MO','AL','KY','GA','MS') THEN 1
+        WHEN datediff(d, '2020-12-01', TO_DATE(contactdate, 'YYYY-MM-DD')) > 0 and voter_state NOT IN ('MN','TN','AR','MO','AL','KY','GA','MS','NH','NV','IA','SC') THEN 1
+        ELSE 0 END as validtime2, 
         COUNT(DISTINCT CASE WHEN support_int = 1 AND unique_id_flag=TRUE THEN person_id END) AS ccj_id_1 ,
         COUNT(DISTINCT CASE WHEN support_int = 1 OR support_int = 2  AND unique_id_flag=TRUE THEN person_id END) AS ccj_id_1_2 ,
         COUNT(DISTINCT CASE WHEN support_int = 2 AND unique_id_flag=TRUE THEN person_id END) AS ccj_id_2 ,
@@ -75,10 +78,10 @@ FROM
         COUNT(DISTINCT CASE WHEN support_int = 4 AND unique_id_flag=TRUE THEN person_id END) AS ccj_id_4 ,
         COUNT(DISTINCT CASE WHEN support_int = 5 AND unique_id_flag=TRUE THEN person_id END) AS ccj_id_5 
         FROM bernie_data_commons.ccj_dnc 
-        WHERE unique_id_flag = 1 AND person_id IS NOT NULL GROUP BY 1,2,3) ccj
+        WHERE unique_id_flag = 1 AND person_id IS NOT NULL GROUP BY 1,2,3,4) ccj
         LEFT JOIN 
-        (select person_id::varchar as pid, 1 as holdout from haystaq.set_no_current where set_no=3) ho2
-        on ccj.person_id = ho2.pid) where validation_id = 1)
+        (select person_id::varchar as pid2, 1 as holdout2 from haystaq.set_no_current where set_no=3) ho2
+        on ccj.person_id = ho2.pid2) where validation_id2 = 1)
 using(person_id)
 
 LEFT JOIN
@@ -94,6 +97,5 @@ UNION ALL
 UNION ALL
 (select person_id::varchar, 'VA Universe' as gotv_univ_state from gotv_universes.VA_gotv_universe_20200124))
 using(person_id)
-));
-
+);
 
