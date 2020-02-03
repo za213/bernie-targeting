a9cbe299c4a6884b6c290f71666dd6d0dfe63723
Nicholas Marchio <namarchio@gmail.com>
2020-02-03 13:29:35 -0500

    Update scores-eval.sql

--- universes/scores-eval.sql
+++ universes/scores-eval.sql
@@ -1,50 +1,51 @@
-
---table to create rankings
-set wlm_query_slot_count to 4;
+-- Standard Recipe
+set wlm_query_slot_count to 3;
 DROP TABLE IF EXISTS bernie_nmarchio2.base_universe_qc;
 CREATE TABLE bernie_nmarchio2.base_universe_qc
 distkey(person_id) 
 sortkey(person_id) as
 (SELECT *, 
-NTILE(100) OVER (PARTITION BY state_code||electorate_2way ORDER BY gotv_validation_rank ASC) AS gotv_tiers_100,
-NTILE(100) OVER (PARTITION BY state_code||electorate_2way ORDER BY gotv_validation_rank_extra ASC) AS gotv_tiers_extra_100 
+NTILE(20) OVER (PARTITION BY state_code||electorate_2way ORDER BY gotv_validation_rank ASC) AS gotv_tiers_20,
+NTILE(20) OVER (PARTITION BY state_code||electorate_2way ORDER BY gotv_validation_rank_extra ASC) AS gotv_tiers_extra_20 
  from
  (select *
-        ,round(1.0*count(*) OVER (partition BY state_code ORDER BY support_guardrail ASC, field_id_1_score DESC ROWS UNBOUNDED PRECEDING)/pturnout_2016,4) AS rolling_electorate_share
-        ,row_number() OVER (PARTITION BY state_code ORDER BY support_guardrail ASC, field_id_1_score DESC) as gotv_validation_rank
+        ,round(1.0*count(*) OVER (partition BY state_code||electorate_2way ORDER BY support_guardrail ASC, field_id_1_score DESC ROWS UNBOUNDED PRECEDING)/pturnout_2016,4) AS rolling_electorate_share
+        ,row_number() OVER (PARTITION BY state_code||electorate_2way ORDER BY support_guardrail ASC, field_id_1_score DESC) as gotv_validation_rank
 
-        ,round(1.0*count(*) OVER (partition BY state_code ORDER BY support_guardrail_extra ASC, field_id_1_score DESC ROWS UNBOUNDED PRECEDING)/pturnout_2016,4) AS rolling_electorate_share_extra
-        ,row_number() OVER (PARTITION BY state_code ORDER BY support_guardrail_extra ASC, field_id_1_score DESC) as gotv_validation_rank_extra
-
+        ,round(1.0*count(*) OVER (partition BY state_code||electorate_2way ORDER BY support_guardrail_extra ASC, field_id_1_score DESC ROWS UNBOUNDED PRECEDING)/pturnout_2016,4) AS rolling_electorate_share_extra
+        ,row_number() OVER (PARTITION BY state_code||electorate_2way ORDER BY support_guardrail_extra ASC, field_id_1_score DESC) as gotv_validation_rank_extra
 from bernie_data_commons.base_universe where state_code in ('AL','AR','CO','ME','MA','MN','NC','OK','TN','TX','UT','VT','VA') ));
 
---score eval queryselect *,
-1.0*ccj1/ccj1all as ccj1rate,
-1.0*ccj12/ccj1all as ccj12rate
+
+-- Scores Eval
+select 
+state_code,
+ccj_contact_group,
+gotv_tiers_20,
+coalesce(number_of_voters,0) as number_of_voters,
+sum(coalesce(number_of_voters,0)) over (partition BY state_code||gotv_tiers_20) as number_of_voters_in_ventile,
+coalesce(activists,0) as activists,
+sum(coalesce(activists,0)) over (partition BY state_code||gotv_tiers_20) as activists_in_ventile,
+round(coalesce(1.0*ccj1/ccj1all,0),4) as ccj1rate,
+round(coalesce(1.0*ccj12/ccj1all,0),4) as ccj12rate,
+coalesce(activists,0) as activists,
+coalesce(ccj1,0) as ccj1,
+coalesce(ccj2,0) as ccj2,
+coalesce(ccj3,0) as ccj3,
+coalesce(ccj4,0) as ccj4,
+coalesce(ccj5,0) as ccj5,
+coalesce(ccj1all,0) as ccj1all,
+coalesce(ccj12,0) as ccj12
 from
 (select
 state_code,
- case --when rolling_electorate_share <= .05 then '05%'
-      when rolling_electorate_share <= .10 then '10%'
-      --when rolling_electorate_share <= .15 then '15%'
-      when rolling_electorate_share <= .20 then '20%'
-      --when rolling_electorate_share <= .25 then '25%'
-      when rolling_electorate_share <= .30 then '30%'
-      --when rolling_electorate_share <= .35 then '35%'
-      when rolling_electorate_share <= .40 then '40%'
-      --when rolling_electorate_share <= .45 then '45%'
-      when rolling_electorate_share <= .50 then '50%'
-      --when rolling_electorate_share <= .55 then '55%'
-      when rolling_electorate_share <= .60 then '60%'
-      --when rolling_electorate_share <= .65 then '65%'
-      when rolling_electorate_share <= .70 then '70%'
-      --when rolling_electorate_share <= .75 then '75%'
-      when rolling_electorate_share <= .80 then '80%'
-      --when rolling_electorate_share <= .85 then '85%'
-      when rolling_electorate_share <= .90 then '90%'
-      --when rolling_electorate_share <= .95 then '95%'
- else 'Above 100%' end as share_of_2016_electorate,
-count(*) as number_of_ids_in_holdout,
+case 
+ when ccj_holdout_id = 1 then 'Holdout' 
+when ccj_holdout_id = 0 then 'Training'
+when ccj_holdout_id is null then 'Uncontacted'
+end as ccj_contact_group,
+gotv_tiers_20,
+count(*) as number_of_voters,
 sum(case when activist_flag = 1
           OR activist_household_flag = 1
           OR donor_1plus_flag = 1 
@@ -59,4 +60,4 @@ sum(ccj_id_1_2) as ccj12,
 avg(field_id_1_score_100) as field1score,
 avg(current_support_raw_100) as supportscore
 from 
-bernie_nmarchio2.base_universe_qc where ccj_holdout_id = 1 group by 1,2) order by 1
+bernie_nmarchio2.base_universe_qc where electorate_2way = '1 - Target universe'  group by 1,2,3) order by 1,2,3
