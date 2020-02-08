604c5dbab556894d99b2d2d1883c31e1dff1404f
Nicholas Marchio <namarchio@gmail.com>
2020-02-08 16:28:27 -0500

    Delete lists_tracking_query.sql

--- universes/in-field-tracker/lists_tracking_query.sql
+++
@@ -1,8 +0,0 @@
-
-
-select person_id, 
-gotv_rank as gotv_rank_order
-'list name' as list_name, 
-'list cutter' as user_name, 
-TO_DATE('2020-02-05', 'YYYY-MM-DD') as date_in_field
-from gotv_universes.ut_listname;
