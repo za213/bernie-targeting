ed6f594260ffea21e51f41fee4712cd877f6ba4f
HarryBakerMD <51973780+HarryBakerMD@users.noreply.github.com>
2020-02-28 21:19:58 -0500

    Update in-field-tracker-condensed.R

--- universes/in-field-tracker/in-field-tracker-condensed.R
+++ universes/in-field-tracker/in-field-tracker-condensed.R
@@ -60,8 +60,7 @@ FROM (
          ,contactcontact_id
          ,contactdate
          ,voter_state
-         ,case when contacttype in ('pdi_mobile', 'bern_app_crowd_canvass','minivan_doors', 'minivan_paid_doors','myc-Paid Walk', 'myc_minivan_doors', 'myv-Paid Walk') then 'canvasses'
-             else contacttype end contacttype
+         ,contacttype
          ,CASE WHEN resultcode IN ('Canvassed', 'Do Not Contact', 'Refused', 'Call Back', 'Language Barrier', 'Hostile', 'Come Back', 'Cultivation', 'Refused Contact', 'Spanish', 'Other', 'Not Interested') THEN 1 ELSE 0 END AS ccj_contact_made
          ,max(TO_DATE(contactdate, 'YYYY-MM-DD')) OVER (PARTITION BY person_id, contacttype) AS most_recent_attempt
          ,max(case when ccj_contact_made = 1 then TO_DATE(contactdate, 'YYYY-MM-DD') else null end) OVER (PARTITION BY person_id, contacttype) AS most_recent_canvass
@@ -74,12 +73,23 @@ FROM (
          ,CASE WHEN support_int = 5 AND mrc = 1 THEN 1 ELSE 0 END AS ccj_id_5
          ,CASE WHEN support_int IN (1,2,3,4,5) AND mrc = 1 THEN 1 ELSE 0 END AS ccj_id_1_2_3_4_5
     FROM (
-        select *,
-               row_number() over (partition by person_id, contacttype, case when support_int is not null then 1 else 0 end order by contacttimestamp desc) mrc
+    select h.*,
+           row_number() over (partition by person_id, contacttype, case when support_int is not null then 1 else 0 end order by contacttimestamp desc) mrc
+    from (
+        select contactcontact_id,
+               person_id,
+               contactdate,
+               voter_state,
+               resultcode,
+               support_int,
+               contacttimestamp,
+           case when contacttype in ('pdi_mobile', 'bern_app_crowd_canvass','minivan_doors', 'minivan_paid_doors','myc-Paid Walk', 'myc_minivan_doors', 'myv-Paid Walk') then 'canvasses'
+             else contacttype end contacttype
         from bernie_data_commons.ccj_dnc
-        ) mr
+        where contacttype in ('pdi_mobile', 'bern_app_crowd_canvass','minivan_doors', 'minivan_paid_doors','myc-Paid Walk', 'myc_minivan_doors', 'myv-Paid Walk', 'getthru_dialer', 'spoke')
+        ) h
+    ) mr
     WHERE person_id IS NOT NULL
-      and contacttype in ('pdi_mobile', 'bern_app_crowd_canvass','minivan_doors', 'minivan_paid_doors','myc-Paid Walk', 'myc_minivan_doors', 'myv-Paid Walk', 'getthru_dialer', 'spoke')
     ) x
 where person_id is not null)"
 
