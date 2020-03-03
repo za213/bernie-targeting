e8ef0b337c73e41cbb14a39d2cc69bfd512c29a3
Nicholas Marchio <namarchio@gmail.com>
2020-03-03 16:53:06 -0600

    Update persuasion-recode.sql

--- pipeline-etl/dv-recode/persuasion-recode.sql
+++ pipeline-etl/dv-recode/persuasion-recode.sql
@@ -1,4 +1,3 @@
-
 drop table if exists bernie_nmarchio2.message_aggregates;
 
 create table bernie_nmarchio2.message_aggregates distkey(campaign_contact_id) sortkey(campaign_contact_id) as
@@ -9,18 +8,18 @@ create table bernie_nmarchio2.message_aggregates distkey(campaign_contact_id) so
 ,case when text_stripped similar to ('%Big Pharma has been ripping off%') then 1 else 0  end as big_pharma_message
 ,case when text_stripped similar to ('health care') then 1 else 0  end as health_care_message
 ,case when text_stripped similar to ('car insurance is unaffordable') then 1 else 0  end as car_insurance_message
-,case when text_stripped similar to ('%Young people are leading the movement%|%building a movement%|%grassroots energy%|%movement of volunteers, donors, and supporters%') then 1 else 0 end as 
-,case when text_stripped similar to ('%HUGE NEWS: Bernie JUST announced%|%running for PRESIDENT%|%Bernie JUST announced%') then 1 else 0 end as annoucement_message
+,case when text_stripped similar to ('%Young people are leading the movement%|%building a movement%|%grassroots energy%|%movement of volunteers, donors, and supporters%') then 1 else 0 end as movement_message
+,case when text_stripped similar to ('%running for PRESIDENT%|%Bernie JUST announced%') then 1 else 0 end as annoucement_message
 ,case when text_stripped similar to ('%Iowa, New Hampshire, Nevada,%') then 1 else 0 end as first_3_wins_message
 ,case when text_stripped similar to ('%Trump%') then 1 else 0 end as beat_trump_message
 ,case when text_stripped similar to ('%siempre ha luchado por la clase trabajadora%|%esforzado toda su vida en luchar por los derechos de los trabajadores%') then 1 else 0 end as fight_for_workers_spanish_message
 ,case when text_stripped similar to ('%fighting against greed and corruption%|%against greed and corruption for decades%') then 1 else 0 end as fight_against_greed_corrupt_message
 ,case when text_stripped similar to ('%corrupt political and economic system that needs major change%') then 1 else 0 end as corrupt_system_needs_major_change_message
 ,case when text_stripped similar to ('%on the side of working people against a corrupt corporate elite%') then 1 else 0 end as working_people_against_corpelite_message
-,case when text_stripped similar to ('%standing up to the greed and corruption of the corporate elite%' then 1 else 0 end as standing_up_to_corpelite
+,case when text_stripped similar to ('%standing up to the greed and corruption of the corporate elite%') then 1 else 0 end as standing_up_to_corpelite_message
 ,case when text_stripped similar to ('%take on the billionaire class%') then 1 else 0 end as take_on_billionaire_class_message
 ,case when text_stripped similar to ('%work for all of us, not just wealthy campaign contributors%') then 1 else 0 end as work_for_all_not_wealthy_message
-,case when text_stripped similar to ('%take on the billionaire class%|%corporate elite%|%wealthy campaign contributors%|%corrupt political%|%greed and corruption%') then 1 else 0 end as strong_class_based_language_message 
+,case when text_stripped similar to ('%take on the billionaire class%|%corporate elite%|%wealthy campaign contributors%|%corrupt political%|%greed and corruption%') then 1 else 0 end as strong_class_based_language_message
  from
 (select *, 
 regexp_replace(text, '((Hi|Hey|Hola|This is).*(!|.|,) ((This is)|(Just wanted)|(quería)|(there\'s)|(It\'s)|(it\'s)|(soy)) ([^.!my]+))') as text_stripped 
