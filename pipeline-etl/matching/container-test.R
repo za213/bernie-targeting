eb096aa4b342e1d9ef848cb0f85e087428135626
Nicholas Marchio <namarchio@gmail.com>
2020-03-03 10:18:56 -0600

    Update container-test.R

--- pipeline-etl/matching/container-test.R
+++ pipeline-etl/matching/container-test.R
@@ -2,11 +2,11 @@
 library(civis)
 library(tidyverse)
 
+input_table_param = Sys.getenv(INPUT_TABLE)
 matches_per_id = Sys.getenv(MATCHES_PER_ID)
 enable_cass = Sys.getenv(ENABLE_CASS)  
 rematch_threshold = Sys.getenv(REMATCH_THRESHOLD)
 cutoff_threshold = Sys.getenv(CUTOFF_THRESHOLD) 
-input_table_param = Sys.getenv(INPUT_TABLE)
 pii_param = Sys.getenv(INPUT_TABLE_PII)
 output_table_param = Sys.getenv(OUTPUT_TABLE)
 
