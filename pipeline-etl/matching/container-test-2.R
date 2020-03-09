b0cc57695dd58522d83d674f6949e08b96a9c771
Nicholas Marchio <namarchio@gmail.com>
2020-03-09 14:24:21 -0500

    Update container-test-2.R

--- pipeline-etl/matching/container-test-2.R
+++ pipeline-etl/matching/container-test-2.R
@@ -5,10 +5,5 @@ MATCHES_PER_ID <- Sys.getenv('MATCHES_PER_ID')
 ENABLE_CASS <- Sys.getenv('ENABLE_CASS')
 CUTOFF_THRESHOLD <- Sys.getenv('CUTOFF_THRESHOLD')
 REMATCH_THRESHOLD <- Sys.getevn('REMATCH_THRESHOLD')
-#INPUT_COLUMN_MAPPING <- Sys.getenv('INPUT_COLUMN_MAPPING')
 
-library(rjson)
-library(stringr)
-library(civis)
-library(tidyverse)
 
