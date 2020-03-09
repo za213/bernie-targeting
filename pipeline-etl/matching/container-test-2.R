8992b11caa47fdfaed2e891d4a4600fa175ff301
Nicholas Marchio <namarchio@gmail.com>
2020-03-09 14:07:24 -0500

    Update container-test-2.R

--- pipeline-etl/matching/container-test-2.R
+++ pipeline-etl/matching/container-test-2.R
@@ -1,6 +1,39 @@
 
-VARIABLE_PARAM <- Sys.getenv('INPUT_TABLE')
+INPUT_TABLE <- Sys.getenv('INPUT_TABLE')
+OUTPUT_TABLE <- Sys.getenv('OUTPUT_TABLE')
+MATCHES_PER_ID <- Sys.getenv('MATCHES_PER_ID')
+ENABLE_CASS <- Sys.getenv('ENABLE_CASS')
+CUTOFF_THRESHOLD <- Sys.getenv('CUTOFF_THRESHOLD')
+#INPUT_COLUMN_MAPPING <- Sys.getenv('INPUT_COLUMN_MAPPING')
 
-cat(VARIABLE_PARAM)
+library(rjson)
+library(stringr)
+library(civis)
+library(tidyverse)
 
-print(VARIABLE_PARAM)
+# Input table parameters
+input_table_param <- stringr::str_split(INPUT_TABLE, '[.]', simplify=TRUE)
+input_schema = input_table_param[1] 
+input_table = input_table_param[2] 
+
+# Output table parameters
+output_table_param <- stringr::str_split(OUTPUT_TABLE, '[.]', simplify=TRUE)
+output_schema = output_table_param[1]
+output_table = output_table_param[2]
+
+# Matching parameters
+matches_per_id = as.integer(MATCHES_PER_ID) # integer, number of matches allowed per source ID (will be deduplicated in output table)
+enable_cass = as.logical(ENABLE_CASS) # boolean, run CASS address standardization
+rematch_threshold = as.numeric(REMATCH_THRESHOLD) # decimal, rematch all records less than this match score on each update (automatically includes new records without scores in input table)
+cutoff_threshold =  as.numeric(CUTOFF_THRESHOLD) # decimal, keep all matches greater than or equal to this match score in final table
+#pii_param <- rjson::fromJSON(INPUT_COLUMN_MAPPING, simplify=FALSE)[1][[1]] # Source table columns
+
+# Assert input params
+stopifnot(matches_per_id >= 1 & matches_per_id <= 10) 
+stopifnot(enable_cass == TRUE | enable_cass == FALSE) 
+stopifnot(rematch_threshold >= 0 & rematch_threshold < 1) 
+stopifnot(cutoff_threshold  >= 0 & cutoff_threshold  < 1 & cutoff_threshold < rematch_threshold) 
+#input_assert_df <- civis::read_civis(x=sql(paste0("select column_name, t.table_schema , t.table_name from information_schema.tables t inner join information_schema.columns c on c.table_name = t.table_name and c.table_schema = t.table_schema where (t.table_schema = '",input_schema,"' and t.table_name = '",input_table,"') order by ordinal_position")), database = 'Bernie 2020')
+#input_col_list <- unique(input_assert_df$column_name)
+#param_col_list <- unlist(compact(pii_param), use.names=FALSE)
+#stopifnot(param_col_list %in% input_col_list)
